e6bb2a79546affd4bbb07c272fdb4a06a32dfa0f
Ken Hawkins <khawkins98@gmail.com>
2019-06-13 14:05:59 +0100

    Update breadcrumb pattern
    resolves https://gitlab.ebi.ac.uk/emblorg/backlog/issues/167

--- wp-content/plugins/vf-breadcrumbs-container/template.php
+++ wp-content/plugins/vf-breadcrumbs-container/template.php
@@ -1,3 +1,5 @@
-<nav class="vf-breadcrumbs embl-breadcrumbs-lookup" aria-label="Breadcrumb" data-embl-js-breadcrumbs-lookup>
-  <div class="vf-text embl-breadcrumbs-lookup--loading-text"></div>
-</nav>
+<div class="embl-grid">
+  <nav class="vf-breadcrumbs embl-breadcrumbs-lookup" aria-label="Breadcrumb" data-embl-js-breadcrumbs-lookup>
+    <div class="vf-text embl-breadcrumbs-lookup--loading-text"></div>
+  </nav>
+</div>
