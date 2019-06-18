77132206afb6b2b55735ec86f42f89f8fdd84290
Stuart Robson <stu@alwaystwisted.com>
2019-06-18 11:49:49 +0100

    removes embl specific divs and classes
    As the masthead has been made more malleable with `vf-header` we no longer need any `embl-` specific CSS or additional classes as it should all 'just work'

--- wp-content/themes/vf-wp/partials/header.php
+++ wp-content/themes/vf-wp/partials/header.php
@@ -1,11 +1,9 @@
 <?php get_template_part('partials/head'); ?>
 <?php vf_header(); ?>
 
-<div class="vf-inlay">
-  <header class="embl-group-header__header embl-group-page--vf-header--bg-color">
-    <?php get_template_part('partials/vf-masthead'); ?>
-    <div class="embl-group-header__navigation embl-group-header__navigation--main">
-      <?php get_template_part('partials/vf-navigation'); ?>
-    </div>
-  </header>
-</div>
+
+<header class="vf-header">
+  <?php get_template_part('partials/vf-masthead'); ?>
+  <?php get_template_part('partials/vf-navigation'); ?>
+</header>
+
