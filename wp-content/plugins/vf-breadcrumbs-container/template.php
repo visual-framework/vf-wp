d9884aa3278b7b0514793c6dc2e606d7812295dd
Stuart Robson <stu@alwaystwisted.com>
2019-06-21 22:04:56 +0100

    the one where Stuart removes the grid wrapper
    This will break unless we edit `vf-breadcrumbs` in vf-core to make `grid-column` go from `1 / -1` to `2 / -2`
    
    (unless I get the overhaul of the grid system in place - then it won't need `grid-column`

--- wp-content/plugins/vf-breadcrumbs-container/template.php
+++ wp-content/plugins/vf-breadcrumbs-container/template.php
@@ -1,5 +1,3 @@
-<div class="embl-grid">
-  <nav class="vf-breadcrumbs embl-breadcrumbs-lookup" aria-label="Breadcrumb" data-embl-js-breadcrumbs-lookup>
-    <div class="vf-text embl-breadcrumbs-lookup--loading-text"></div>
-  </nav>
-</div>
+<nav class="vf-breadcrumbs embl-breadcrumbs-lookup" aria-label="Breadcrumb" data-embl-js-breadcrumbs-lookup>
+  <div class="vf-text embl-breadcrumbs-lookup--loading-text"></div>
+</nav>
