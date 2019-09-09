ea1d72d6d1ba4c71e59bdf04e14f3a8d2e84cf3c
David Bushell <david@dbushell.com>
2019-09-09 08:02:34 +0100

    move assets to theme directory

--- gulpfile.js
+++ gulpfile.js
@@ -7,28 +7,24 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
-const yaml = require('js-yaml');
 const chalk = require('chalk');
 
-// Load Yaml config and resolve paths
-const config = yaml.safeLoad(fs.readFileSync('vf-wp.yml', 'utf8'));
-for (let key of Object.keys(config)) {
-  if (/_path$/.test(key)) {
-    config[`${key.replace('_relative_', '_')}`] = path.resolve(
-      __dirname,
-      config[key]
-    );
-  }
-}
+const config = {
+  content_path: path.resolve(__dirname, 'wp-content')
+};
+
+config.theme_path = path.resolve(config.content_path, 'themes/vf-wp');
+config.assets_path = path.resolve(config.theme_path, 'assets');
 
 // Setup more paths
 config.content_glob = path.join(config.content_path, '**/*');
 config.style_path = path.join(config.theme_path, 'style.css');
 config.sass_glob = [path.join(config.assets_path, 'scss/**/*.scss')];
+
 const js_glob = path.join(config.assets_path, 'js/*.js');
 config.js_glob = [js_glob, '!' + js_glob.replace(/\.js$/, '.min.js')];
 
-const vf_path = path.join(__dirname, '../vf-core/');
+// const vf_path = path.join(__dirname, '../vf-core/');
 
 // Compile and prefix Sass
 gulp.task('css', callback => {
@@ -36,22 +32,22 @@ gulp.task('css', callback => {
     [
       gulp.src(config.sass_glob),
       sass({
-        outputStyle: 'expanded',
-        includePaths: [
-          vf_path,
-          path.join(vf_path, 'assets/scss'),
-          path.join(vf_path, 'components/utilities'),
-          path.join(vf_path, 'components/elements'),
-          path.join(vf_path, 'components/blocks'),
-          path.join(vf_path, 'components/containers'),
-          path.join(vf_path, 'components/grids')
-        ]
+        outputStyle: 'expanded'
+        // includePaths: [
+        //   vf_path,
+        //   path.join(vf_path, 'assets/scss'),
+        //   path.join(vf_path, 'components/utilities'),
+        //   path.join(vf_path, 'components/elements'),
+        //   path.join(vf_path, 'components/blocks'),
+        //   path.join(vf_path, 'components/containers'),
+        //   path.join(vf_path, 'components/grids')
+        // ]
       }),
       autoprefixer({
         grid: true,
         remove: false
       }),
-      gulp.dest(path.join(config.theme_path, 'assets/css'))
+      gulp.dest(path.join(config.assets_path, 'css'))
     ],
     callback
   );
@@ -105,6 +101,7 @@ gulp.task('update-theme-version', callback => {
 
 // Pre-commit Git hook
 gulp.task('pre-commit', gulp.series('update-theme-version'));
+gulp.task('post-commit', f => f());
 
 // Watch
 gulp.task('watch-css', () => gulp.watch(config.sass_glob, gulp.series('css')));
---
+++ wp-content/themes/vf-wp/assets/js/head.js
@@ -0,0 +1,8 @@
+(function() {
+  // Toggle JavaScript class in <html>
+  const $html = document.getElementsByTagName('html')[0];
+  $html.className = $html.className.replace(
+    /(^|\s)vf-no-js(\s|$)/,
+    '$1vf-js$2'
+  );
+})();
