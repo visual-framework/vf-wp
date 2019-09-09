1a273c35e4439a7f5b28bf1ea7c89914b321a27b
David Bushell <david@dbushell.com>
2019-09-09 10:26:57 +0100

    webpack block JSX build task

--- gulpfile.js
+++ gulpfile.js
@@ -7,16 +7,20 @@ const rename = require('gulp-rename');
 const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const babel = require('gulp-babel');
+const webpack = require('webpack');
 const chalk = require('chalk');
 
+/**
+ * Setup configuration paths
+ */
 const config = {
   content_path: path.resolve(__dirname, 'wp-content')
 };
 
 config.theme_path = path.resolve(config.content_path, 'themes/vf-wp');
+config.plugin_path = path.resolve(config.content_path, 'plugins');
 config.assets_path = path.resolve(config.theme_path, 'assets');
 
-// Setup more paths
 config.content_glob = path.join(config.content_path, '**/*');
 config.style_path = path.join(config.theme_path, 'style.css');
 config.sass_glob = [path.join(config.assets_path, 'scss/**/*.scss')];
@@ -24,9 +28,66 @@ config.sass_glob = [path.join(config.assets_path, 'scss/**/*.scss')];
 const js_glob = path.join(config.assets_path, 'js/*.js');
 config.js_glob = [js_glob, '!' + js_glob.replace(/\.js$/, '.min.js')];
 
-// const vf_path = path.join(__dirname, '../vf-core/');
+/**
+ * Webpack configuration for VF Gutenberg plugin blocks
+ */
+config.vf_blocks_path = path.resolve(
+  config.plugin_path,
+  'vf-gutenberg/assets/vf-blocks.jsx'
+);
 
-// Compile and prefix Sass
+config.vf_blocks_webpack = {
+  entry: config.vf_blocks_path,
+  output: {
+    path: path.resolve(config.plugin_path, 'vf-gutenberg/assets'),
+    filename: 'vf-blocks.min.js'
+  },
+  externals: {
+    wp: 'wp',
+    react: 'React',
+    'react-dom': 'ReactDOM'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  corejs: 3,
+                  useBuiltIns: 'usage',
+                  targets: {
+                    browsers: ['> 1%']
+                  }
+                }
+              ],
+              [
+                '@babel/preset-react',
+                {
+                  pragma: 'wp.element.createElement'
+                }
+              ]
+            ],
+            plugins: []
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+};
+
+/**
+ * Compile and prefix Sass
+ * TODO: remove VF includes - not necessary?
+ */
+// const vf_path = path.join(__dirname, '../vf-core/');
 gulp.task('css', callback => {
   pump(
     [
@@ -53,7 +114,9 @@ gulp.task('css', callback => {
   );
 });
 
-// Compress and minify JavaScript
+/**
+ * Compress and minify JavaScript from theme directory
+ */
 gulp.task('js', callback => {
   pump(
     [
@@ -75,8 +138,10 @@ gulp.task('js', callback => {
   );
 });
 
-// Copy the NPM package version into the theme stylesheet
-// WordPress uses the theme style.css front comment
+/**
+ * Copy the NPM package version into the theme stylesheet
+ * WordPress uses the theme style.css front comment
+ */
 gulp.task('update-theme-version', callback => {
   console.log(chalk.blue('Updating theme version to match package...'));
   if (!fs.existsSync(config.style_path)) {
@@ -92,11 +157,37 @@ gulp.task('update-theme-version', callback => {
   callback();
 });
 
-// Watch
+/**
+ * Build VF Gutenberg blocks plugin
+ */
+gulp.task('vf-blocks', () => {
+  return new Promise((resolve, reject) => {
+    webpack(config.vf_blocks_webpack, (err, stats) => {
+      if (err) {
+        return reject(err);
+      }
+      if (stats.hasErrors()) {
+        return reject(new Error(stats.compilation.errors.join('\n')));
+      }
+      resolve();
+    });
+  });
+});
+
+/**
+ * Watch tasks
+ */
 gulp.task('watch-css', () => gulp.watch(config.sass_glob, gulp.series('css')));
 gulp.task('watch-js', () => gulp.watch(config.js_glob, gulp.series('js')));
+gulp.task('watch-blocks', () =>
+  gulp.watch(config.vf_blocks_path, gulp.series('vf-blocks'))
+);
+gulp.task('watch', gulp.parallel('watch-css', 'watch-js', 'watch-blocks'));
 
-gulp.task('watch', gulp.parallel('watch-css', 'watch-js'));
-
-// Default
-gulp.task('default', gulp.series(gulp.parallel('css', 'js'), 'watch'));
+/**
+ * Default task
+ */
+gulp.task(
+  'default',
+  gulp.series(gulp.parallel('css', 'js', 'vf-blocks'), 'watch')
+);
---
+++ wp-content/plugins/vf-gutenberg/assets/vf-blocks.jsx
@@ -0,0 +1,5 @@
+const {__} = wp.i18n;
+const {registerBlockType} = wp.blocks;
+const {InnerBlocks, RichText, InspectorControls} = wp.editor;
+const {SelectControl, PanelBody, ToggleControl, Button} = wp.components;
+const {Component, createElement} = wp.element;
