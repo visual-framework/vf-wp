88c6ab4c1aaea02dbc86e890e57f9997c086a5ca
David Bushell <david@dbushell.com>
2019-09-30 11:46:05 +0100

    move vf-ebi-global-footer scripts from template to `wp_enqueue_scripts`

--- wp-content/plugins/vf-ebi-global-footer-container/template.php
+++ wp-content/plugins/vf-ebi-global-footer-container/template.php
@@ -10,17 +10,4 @@ if ( ! empty($content)) {
 <div <?php $vf_plugin->api_attr(array('class' => 'vfwp-column-reset')); ?>>
   <?php echo $content; ?>
 </div>
-
-<?php
-// We load these scripts here as a short term solution to not over-architect
-// something that shouldn't be permananent.
-// This also means that for the EBI Header to function, the EBI Footer must
-// also be used.
-?>
-
-<script defer="defer" src="https://dev.ebi.emblstatic.net/web_guidelines/EBI-Framework/v1.3/js/script.js"></script>
-
-<link rel="stylesheet" href="https://dev.ebi.emblstatic.net/web_guidelines/EBI-Framework/v1.3/css/ebi-global.css" type="text/css" media="all" />
-<link rel="stylesheet" href="https://dev.ebi.emblstatic.net/web_guidelines/EBI-Icon-fonts/v1.3/fonts.css" type="text/css" media="all" />
-
 <?php } ?>
