ab09c9890d7f249e77140fc71a9ebf4516e8468a
David Bushell <david@dbushell.com>
2019-10-16 11:23:07 +0100

    add full-width page template for theme

---
+++ wp-content/themes/vf-wp/template-full-width.php
@@ -0,0 +1,25 @@
+<?php
+/**
+* Template Name: Full-width page
+*/
+
+get_template_part('partials/header');
+
+the_post();
+
+?>
+<section class="vf-inlay">
+  <div class="vf-inlay__content vf-u-background-color-ui--white">
+    <main class="vf-inlay__content--full-width">
+      <h1 class="vf-text vf-text--display-l"><?php the_title(); ?></h1>
+
+      <?php the_content(); ?>
+
+    </main>
+  </div>
+</section>
+<?php
+
+get_template_part('partials/footer');
+
+?>
