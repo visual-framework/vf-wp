02ac68a8da986211730f459807c6223fb570de4f
Ken Hawkins <khawkins98@gmail.com>
2019-10-18 09:56:27 +0200

    Boring setup

--- wp-content/themes/vf-wp-news/functions.php
+++ wp-content/themes/vf-wp-news/functions.php
@@ -1,6 +1,17 @@
 <?php 
-	 add_action( 'wp_enqueue_scripts', 'vf_wp_news_enqueue_styles' );
-	 function vf_wp_news_enqueue_styles() {
- 		  wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
- 		  } 
- ?>
+	if( ! defined( 'ABSPATH' ) ) exit;
+
+	add_action( 'wp_enqueue_scripts', 'vf_wp_news_enqueue_styles' );
+		function vf_wp_news_enqueue_styles() {
+		wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+	} 
+
+	// require_once('functions/walker-comment.php');
+	// require_once('functions/helpers.php');
+	require_once('functions/theme.php');
+	// require_once('functions/admin.php');
+	// require_once('functions/widgets.php');
+	// require_once('functions/pagination.php');
+	// require_once('functions/comment-form.php');
+
+?>
