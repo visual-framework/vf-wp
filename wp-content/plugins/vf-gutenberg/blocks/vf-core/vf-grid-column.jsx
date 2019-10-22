f9625821c8cda60bfbef7d6666d30e41710fc3a3
David Bushell <david@dbushell.com>
2019-10-22 14:46:18 +0100

    add vf/grid column span attribute (wip)

--- wp-content/plugins/vf-gutenberg/blocks/vf-core/vf-grid-column.jsx
+++ wp-content/plugins/vf-gutenberg/blocks/vf-core/vf-grid-column.jsx
@@ -1,9 +1,11 @@
 /**
 Block Name: Grid Column
 */
-import React from 'react';
-import {InnerBlocks} from '@wordpress/block-editor';
-import {select} from '@wordpress/data';
+import React, {Fragment} from 'react';
+import {InnerBlocks, InspectorControls} from '@wordpress/block-editor';
+import {PanelBody, RangeControl} from '@wordpress/components';
+import {withDispatch, withSelect} from '@wordpress/data';
+import {compose} from '@wordpress/compose';
 import {__} from '@wordpress/i18n';
 import useVFDefaults from '../hooks/use-vf-defaults';
 
@@ -21,38 +23,102 @@ const settings = {
   supports: {
     ...defaults.supports,
     inserter: false
+  },
+  attributes: {
+    span: {
+      type: 'integer',
+      default: 1
+    }
   }
 };
 
+const withCompose = compose(
+  withSelect((select, ownProps) => {
+    const {
+      getBlockAttributes,
+      getBlockName,
+      getBlockOrder,
+      getBlockRootClientId
+    } = select('core/block-editor');
+
+    // Get parent grid properties
+    const gridClientId = getBlockRootClientId(ownProps.clientId);
+    const gridAttrs = getBlockAttributes(gridClientId);
+
+    // Set column block properties
+    const hasChildBlocks = getBlockOrder(ownProps.clientId).length > 0;
+    const isVFGrid = getBlockName(gridClientId) === 'vf/grid';
+
+    return {
+      hasChildBlocks,
+      isVFGrid,
+      isEMBLGrid: !isVFGrid,
+      gridColumns: gridAttrs.columns
+    };
+  }),
+  withDispatch((dispatch, ownProps, {select}) => {
+    const setSpan = (prevSpan, newSpan) => {
+      ownProps.setAttributes({span: newSpan});
+    };
+    return {
+      setSpan
+    };
+  })
+);
+
 settings.save = props => {
+  const {span} = props.attributes;
+  const attrs = {};
+  if (span > 1) {
+    attrs.className = `vf-grid__col--span-${span}`;
+  }
   return (
-    <div>
+    <div {...attrs}>
       <InnerBlocks.Content />
     </div>
   );
 };
 
-const withGridProps = Edit => {
-  return props => {
-    const {getBlockOrder} = select('core/block-editor');
-    const hasChildBlocks = getBlockOrder(props.clientId).length > 0;
-    return Edit({...props, hasChildBlocks});
-  };
-};
+settings.edit = withCompose(props => {
+  const {gridColumns, hasChildBlocks, isVFGrid} = props;
+  const {span} = props.attributes;
 
-settings.edit = withGridProps(props => {
+  // Ensure version is encoded in post content
   props.setAttributes({ver: props.ver || ver});
+
+  // Setup inspector tools
+  let inspector = false;
+  if (isVFGrid && gridColumns > 2) {
+    inspector = (
+      <InspectorControls>
+        <PanelBody title={__('Settings')} initialOpen>
+          <RangeControl
+            label={__('Column span')}
+            help={__('Experimental feature.')}
+            value={span}
+            onChange={value => props.setSpan(span, value)}
+            min={1}
+            max={gridColumns - 1}
+          />
+        </PanelBody>
+      </InspectorControls>
+    );
+  }
+
   return (
-    <div className="vf-block-column">
-      <InnerBlocks
-        templateLock={false}
-        renderAppender={
-          props.hasChildBlocks
-            ? undefined
-            : () => <InnerBlocks.ButtonBlockAppender />
-        }
-      />
-    </div>
+    <Fragment>
+      {inspector}
+      <div className="vf-block-column">
+        <InnerBlocks
+          templateLock={false}
+          renderAppender={
+            hasChildBlocks
+              ? undefined
+              : () => <InnerBlocks.ButtonBlockAppender />
+          }
+        />
+      </div>
+    </Fragment>
   );
 });
 
