baaf7fb0b86ec029de161f9a59e26ed76481797e
David Bushell <david@dbushell.com>
2019-11-06 09:31:48 +0000

    vf-wp-groups front-page template

--- wp-content/themes/vf-wp-groups/front-page.php
+++ wp-content/themes/vf-wp-groups/front-page.php
@@ -10,13 +10,18 @@ get_header();
 
 global $vf_theme;
 
+// Marker appended after each inlay section
+$placeholder = "\n<!--[/VF_INLAY]-->\n";
+
+// These blocks require no wrapper; their templates have layout
 $has_wrap = array(
-  'acf/vf-latest-posts',
-  'acf/vf-group-header',
+  'acf/vf-latest-posts', // deprecated
+  'acf/vf-group-header', // deprecated
   'vf/latest-posts',
   'vf/group-header',
 );
 
+// Prepend the opening wrapper elements
 $open_wrap = function($html = '') {
   return '
 <section class="vf-inlay">
@@ -25,46 +30,42 @@ $open_wrap = function($html = '') {
   ' . $html;
 };
 
-$close_wrap = function($html = '') {
-  return $html . '
+// Append the closing wrapper elements
+$close_wrap = function($html = '') use ($placeholder) {
+  return $html . "
     </main>
   </div>
 </section>
-<!--[/VF_INLAY]-->
-  ';
+{$placeholder}";
 };
 
+// Render block callback
 $render_block = function($block_html, $block_name)
-  use ($has_wrap, $open_wrap, $close_wrap)
+  use ($has_wrap, $open_wrap, $close_wrap, $placeholder)
 {
-  // Append placeholder
-  $placeholder = "\n<!--[/VF_INLAY]-->\n";
+  // 1) Append placeholder (block has its own template)
   if (in_array($block_name, $has_wrap)) {
     return $block_html . $placeholder;
   }
-  // Add wrapper and placeholder if block isn't going to be wrapped
+  // 2) Add wrapper (block is not wrapped)
   if ( ! in_array($block_name, VF_Theme_Content::WRAPPED)) {
-    return
-        $open_wrap()
-      . $block_html
-      . $close_wrap()
-      . $placeholder;
+    return $open_wrap() . $block_html . $close_wrap();
   }
+  // 3) Return unchanged (block is wrapped by filters)
   return $block_html;
 };
 
+// Add filters
 add_filter(
   'vf/theme/content/open_block_wrap',
   $open_wrap,
   10, 1
 );
-
 add_filter(
   'vf/theme/content/close_block_wrap',
   $close_wrap,
   10, 1
 );
-
 add_filter(
   'vf/theme/content/render_block',
   $render_block,
@@ -78,7 +79,7 @@ $html = ob_get_contents();
 ob_end_clean();
 
 // Add VF divider patterns and output content
-$inlays = explode('<!--[/VF_INLAY]-->', $html);
+$inlays = explode(trim($placeholder), $html);
 foreach ($inlays as $i => $html) {
   if ($i !== 0) {
     $html = preg_replace(
