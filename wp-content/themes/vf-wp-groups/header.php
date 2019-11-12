de8a6702858444d4b3ba356b898f47e755f59ddf
David Bushell <david@dbushell.com>
2019-11-12 09:06:41 +0000

    vf-wp-groups vf-header container templates

--- wp-content/themes/vf-wp-groups/header.php
+++ wp-content/themes/vf-wp-groups/header.php
@@ -1,6 +1,3 @@
 <?php get_template_part('partials/head'); ?>
 <?php vf_header(); ?>
-<header class="vf-header vf-header--inlay">
-  <?php get_template_part('partials/vf-masthead'); ?>
-  <?php get_template_part('partials/vf-navigation'); ?>
-</header>
+<?php get_template_part('vf-header/vf-header'); ?>
---
+++ wp-content/themes/vf-wp-groups/vf-header/vf-header.php
@@ -0,0 +1,4 @@
+<header class="vf-header vf-header--inlay">
+  <?php get_template_part('vf-header/vf-masthead'); ?>
+  <?php get_template_part('vf-header/vf-navigation'); ?>
+</header>
