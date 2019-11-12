2699d2e4413da0aaf139353a19aa125efab6c482
David Bushell <david@dbushell.com>
2019-11-12 09:39:18 +0000

    groups theme functions

---
+++ wp-content/themes/vf-wp-groups/functions/groups-customize.php
@@ -0,0 +1,17 @@
+<?php
+
+if( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists('VF_Groups_Customize') ) :
+
+class VF_Groups_Customize {
+
+  public function __construct() {
+    // Nothing...
+  }
+
+} // VF_Groups_Customize
+
+endif;
+
+?>
