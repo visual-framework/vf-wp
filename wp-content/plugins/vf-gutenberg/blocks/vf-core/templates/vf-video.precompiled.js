aa2250b03f1a81e23f81deaea498a5541dcbe24f
David Bushell <david@dbushell.com>
2019-11-25 13:55:19 +0000

    vf-video gutenberg block aspect ratio attribute (#80)

--- wp-content/plugins/vf-gutenberg/blocks/vf-core/templates/vf-video.precompiled.js
+++ wp-content/plugins/vf-gutenberg/blocks/vf-core/templates/vf-video.precompiled.js
@@ -1,49 +1,50 @@
 /**
  * Precompiled Nunjucks template: vf-video.njk
 
-<div class="vf-video">
+{% if src %}
+<div class="vf-video"{% if padding %} style="padding-bottom: {{ padding }}%"{% endif %}>
     <iframe width="{{ width }}" height="{{ height }}" src="{{ src }}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
 </div>
+{% endif %}
 
  */
-(function() {
-  (window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})[
-    'vf-video'
-  ] = (function() {
-    function root(env, context, frame, runtime, cb) {
-      var lineno = 0;
-      var colno = 0;
-      var output = '';
-      try {
-        var parentTemplate = null;
-        output += '<div class="vf-video">\n    <iframe width="';
-        output += runtime.suppressValue(
-          runtime.contextOrFrameLookup(context, frame, 'width'),
-          env.opts.autoescape
-        );
-        output += '" height="';
-        output += runtime.suppressValue(
-          runtime.contextOrFrameLookup(context, frame, 'height'),
-          env.opts.autoescape
-        );
-        output += '" src="';
-        output += runtime.suppressValue(
-          runtime.contextOrFrameLookup(context, frame, 'src'),
-          env.opts.autoescape
-        );
-        output +=
-          '" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>\n</div>';
-        if (parentTemplate) {
-          parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
-        } else {
-          cb(null, output);
-        }
-      } catch (e) {
-        cb(runtime.handleError(e, lineno, colno));
-      }
-    }
-    return {
-      root: root
-    };
-  })();
+(function() {(window.nunjucksPrecompiled = window.nunjucksPrecompiled || {})["vf-video"] = (function() {
+function root(env, context, frame, runtime, cb) {
+var lineno = 0;
+var colno = 0;
+var output = "";
+try {
+var parentTemplate = null;
+if(runtime.contextOrFrameLookup(context, frame, "src")) {
+output += "\n<div class=\"vf-video\"";
+if(runtime.contextOrFrameLookup(context, frame, "padding")) {
+output += " style=\"padding-bottom: ";
+output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "padding"), env.opts.autoescape);
+output += "%\"";
+;
+}
+output += ">\n    <iframe width=\"";
+output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "width"), env.opts.autoescape);
+output += "\" height=\"";
+output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "height"), env.opts.autoescape);
+output += "\" src=\"";
+output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "src"), env.opts.autoescape);
+output += "\" frameborder=\"0\" allow=\"autoplay; encrypted-media\" allowfullscreen></iframe>\n</div>\n";
+;
+}
+if(parentTemplate) {
+parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
+} else {
+cb(null, output);
+}
+;
+} catch (e) {
+  cb(runtime.handleError(e, lineno, colno));
+}
+}
+return {
+root: root
+};
+
+})();
 })();
--- wp-content/plugins/vf-gutenberg/blocks/vf-core/vf-video.jsx
+++ wp-content/plugins/vf-gutenberg/blocks/vf-core/vf-video.jsx
@@ -8,6 +8,19 @@ import useVFCoreSettings from '../hooks/use-vf-core-settings';
 import template from './templates/vf-video.precompiled';
 // import {fromCore} from './transforms/video';
 
+const withRatioPadding = Edit => {
+  return props => {
+    const transient = {...(props.transient || {})};
+    const ratio = props.attributes.ratio || '';
+    const pattern = /(\d+):(\d+)/;
+    if (pattern.test(ratio)) {
+      const [, r1, r2] = ratio.match(pattern);
+      transient.padding = (100 / r1) * r2;
+    }
+    return Edit({...props, transient});
+  };
+};
+
 export default useVFCoreSettings({
   name: 'vf/video',
   title: __('Video / Embed'),
@@ -26,15 +39,33 @@ export default useVFCoreSettings({
     height: {
       type: 'integer',
       default: 360
+    },
+    ratio: {
+      type: 'string',
+      default: ''
     }
   },
   fields: [
     {
       name: 'url',
       control: 'url',
-      label: 'URL',
+      label: __('URL'),
       disableSuggestions: true
+    },
+    {
+      name: 'ratio',
+      control: 'select',
+      label: __('Aspect Ratio'),
+      inspector: true,
+      options: [
+        {label: __('Widescreen (16:9)'), value: '16:9'},
+        {label: __('Standard (4:3)'), value: '4:3'},
+        {label: __('Square (1:1)'), value: '1:1'}
+      ]
     }
   ],
-  withHOC: [[withTransientAttributeMap, [{from: 'url', to: 'src'}]]]
+  withHOC: [
+    [withRatioPadding],
+    [withTransientAttributeMap, [{from: 'url', to: 'src'}]]
+  ]
 });
