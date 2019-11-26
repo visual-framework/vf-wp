f69f7ac369457128fb4a8790256d6bdd7387b3f1
David Bushell <david@dbushell.com>
2019-11-26 11:56:49 +0000

    bump version

--- wp-content/plugins/vf-gutenberg/vf-gutenberg.php
+++ wp-content/plugins/vf-gutenberg/vf-gutenberg.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: VF-WP Gutenberg
 Description: Adds Visual Framework support and blocks to the Gutenberg editor.
-Version: 0.1.0
+Version: 0.1.1
 Author: EMBL-EBI Web Development
 Plugin URI: https://github.com/visual-framework/vf-wp
 Text Domain: vfwp
