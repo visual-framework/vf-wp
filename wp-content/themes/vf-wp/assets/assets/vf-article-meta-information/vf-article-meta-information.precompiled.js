7257940819cdadb4f0f3650b278972753b60d274
David Bushell <david@dbushell.com>
2020-04-08 16:14:58 +0100

    update vf-core styles

--- wp-content/themes/vf-wp/assets/assets/vf-article-meta-information/vf-article-meta-information.precompiled.js
+++ wp-content/themes/vf-wp/assets/assets/vf-article-meta-information/vf-article-meta-information.precompiled.js
@@ -8,7 +8,7 @@ var colno = 0;
 var output = "";
 try {
 var parentTemplate = null;
-output += "<aside class=\"vf-article-meta-information\">\n  <div class=\"vf-author | vf-article-meta-info__author\">\n    <p class=\"vf-author__name | vf-text-body--6\">\n      <a class=\"vf-link\" href=\"";
+output += "<aside class=\"vf-article-meta-information\">\n  <div class=\"vf-author | vf-article-meta-info__author\">\n    <p class=\"vf-author__name\">\n      <a class=\"vf-link\" href=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "meta__url"), env.opts.autoescape);
 output += "\">";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "author__name"), env.opts.autoescape);
@@ -16,7 +16,9 @@ output += "</a>\n    </p>\n   <a class=\"vf-author--avatar__link | vf-link\" hre
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "meta__url"), env.opts.autoescape);
 output += "\">\n     <!-- wrapping the avatar in a link is optional -->\n     <img class=\"vf-author--avatar\" src=\"";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "author__avatar"), env.opts.autoescape);
-output += "\" alt=\"FirstName Surname\" />\n   </a>\n  </div>\n  <div class=\"vf-meta__details\">\n    <p class=\"vf-meta__topics | vf-text-body--6\">Topic:\n";
+output += "\" alt=\"FirstName Surname\" />\n   </a>\n  </div>\n  <div class=\"vf-meta__details\">\n    <p class=\"vf-meta__date\">";
+output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "meta__date"), env.opts.autoescape);
+output += "</p>\n\n    <p class=\"vf-meta__topics\">Topics:\n";
 frame = frame.push();
 var t_3 = runtime.contextOrFrameLookup(context, frame, "meta__topics");
 if(t_3) {t_3 = runtime.fromIterator(t_3);
@@ -38,13 +40,13 @@ output += "</a>\n";
 }
 }
 frame = frame.pop();
-output += "    <p class=\"vf-meta__date | vf-text-body--6\">";
-output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "meta__date"), env.opts.autoescape);
-output += "</p>\n  </div>\n  <div class=\"vf-meta__comments\">\n    <p class=\"vf-text-body--6\">Comments — <a href=\"JavaScript:Void(0);\" class=\"vf-link\">";
+output += "  </div>\n  <div class=\"vf-meta__comments\">\n    <p class\"vf-meta__text\">Comments — <a href=\"JavaScript:Void(0);\" class=\"vf-link\">";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "meta__comment_count"), env.opts.autoescape);
-output += "</a></p>\n  </div>\n  <div class=\"vf-links | vf-article-meta-info__links\">\n    <h6 class=\"vf-text vf-text-body--6\">";
+output += "</a></p>\n  </div>\n";
+if(runtime.contextOrFrameLookup(context, frame, "meta__links")) {
+output += "  <div class=\"vf-links vf-links--tight vf-links__list--s\">\n    <p class=\"vf-links__heading\">";
 output += runtime.suppressValue(runtime.contextOrFrameLookup(context, frame, "meta__links_title"), env.opts.autoescape);
-output += "</h6>\n";
+output += "</p>\n    <ul class=\"vf-links__list vf-links__list--secondary | vf-list\">\n";
 frame = frame.push();
 var t_7 = runtime.contextOrFrameLookup(context, frame, "meta__links");
 if(t_7) {t_7 = runtime.fromIterator(t_7);
@@ -59,14 +61,19 @@ frame.set("loop.revindex0", t_6 - t_5 - 1);
 frame.set("loop.first", t_5 === 0);
 frame.set("loop.last", t_5 === t_6 - 1);
 frame.set("loop.length", t_6);
-output += "      <a href=\"JavaScript:Void(0);\" class=\"vf-link | vf-text-body--6\">";
-output += runtime.suppressValue(t_8, env.opts.autoescape);
-output += "</a>\n";
+output += "      <li class=\"vf-list__item\">\n        <a class=\"vf-list__link\" href=\"";
+output += runtime.suppressValue(runtime.memberLookup((t_8),"url"), env.opts.autoescape);
+output += "\">";
+output += runtime.suppressValue(runtime.memberLookup((t_8),"text"), env.opts.autoescape);
+output += "</a>\n      </li>\n";
 ;
 }
 }
 frame = frame.pop();
-output += "  </div>\n</aside>\n";
+output += "    </ul>\n  </div>\n";
+;
+}
+output += "</aside>\n";
 if(parentTemplate) {
 parentTemplate.rootRenderFunc(env, context, frame, runtime, cb);
 } else {
