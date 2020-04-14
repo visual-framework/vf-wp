42afdc138bdd2c5b3708c48982732a7cef0c72bd
David Bushell <david@dbushell.com>
2020-04-14 09:32:15 +0100

    add `vf-events-type` plugin dir

---
+++ wp-content/plugins/vf-events-type/index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+Plugin Name: VF-WP Events
+Description: VF-WP events custom post type and Gutenberg blocks.
+Version: 0.1.0
+Author: EMBL-EBI Web Development
+Plugin URI: https://github.com/visual-framework/vf-wp
+Text Domain: vfwp
+*/
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+?>
