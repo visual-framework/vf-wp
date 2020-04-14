679ce4e533bca43a38c5988c73343f8dd57871b7
David Bushell <david@dbushell.com>
2020-04-14 12:58:05 +0100

    fix default theme grid

--- wp-content/themes/vf-wp/index.php
+++ wp-content/themes/vf-wp/index.php
@@ -7,34 +7,33 @@ global $vf_theme;
 $title = $vf_theme->get_title();
 
 ?>
-<main class="vf-body">
-  <section class="vf-intro | embl-grid embl-grid--has-centered-content">
-    <div></div>
-    <div>
-      <h1 class="vf-intro__heading"><?php echo esc_html($title); ?></h1>
-    </div>
-    <div></div>
-  </section>
-  <!--/embl-grid-->
-  <section class="embl-grid embl-grid--has-centered-content">
-    <div></div>
-    <div>
-    <?php
-    while (have_posts()) {
-      the_post();
-      get_template_part('partials/vf-summary--article');
-      if ( ! $vf_theme->is_last_post()) {
-        echo '<hr class="vf-divider">';
-      }
+
+<section class="vf-intro | embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+    <h1 class="vf-intro__heading"><?php echo esc_html($title); ?></h1>
+  </div>
+  <div></div>
+</section>
+<!--/embl-grid-->
+<section class="embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+  <?php
+  while (have_posts()) {
+    the_post();
+    get_template_part('partials/vf-summary--article');
+    if ( ! $vf_theme->is_last_post()) {
+      echo '<hr class="vf-divider">';
     }
-    vf_pagination();
-    ?>
-    </div>
-    <div></div>
-  </section>
-  <!--/embl-grid-->
-</main>
-<!--/vf-body-->
+  }
+  vf_pagination();
+  ?>
+  </div>
+  <div></div>
+</section>
+<!--/vf-intro-->
+
 <?php
 
 get_footer();
--- wp-content/themes/vf-wp/singular.php
+++ wp-content/themes/vf-wp/singular.php
@@ -38,34 +38,33 @@ add_filter(
 );
 
 ?>
-<main class="vf-body">
-  <section class="vf-intro | embl-grid embl-grid--has-centered-content">
-    <div></div>
-    <div>
-      <h1 class="vf-intro__heading"><?php the_title(); ?></h1>
-    </div>
-    <div></div>
-  </section>
-  <!--/embl-grid-->
 
-  <?php
+<section class="vf-intro | embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+    <h1 class="vf-intro__heading"><?php the_title(); ?></h1>
+  </div>
+  <div></div>
+</section>
+<!--/vf-intro-->
 
-  $vf_theme->the_content();
+<?php
 
-  ?>
+$vf_theme->the_content();
 
-  <?php if (comments_open() || get_comments_number()) { ?>
-  <section class="embl-grid embl-grid--has-centered-content">
-    <div></div>
-    <div>
-      <?php comments_template(); ?>
-    </div>
-    <div></div>
-  </section>
-  <!--/embl-grid-->
-  <?php } ?>
-</main>
-<!--/vf-body-->
+?>
+
+<?php if (comments_open() || get_comments_number()) { ?>
+<section class="embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+    <?php comments_template(); ?>
+  </div>
+  <div></div>
+</section>
+<!--/embl-grid-->
+<?php } ?>
+
 <?php
 
 get_footer();
