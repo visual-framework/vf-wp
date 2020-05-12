624401b3ff83281c5c19b9dd0a0963811a225c97
David Bushell <david@dbushell.com>
2020-05-12 07:14:00 +0100

    #284 upgrade EBI containers

--- wp-content/plugins/vf-ebi-global-footer-container/template.php
+++ wp-content/plugins/vf-ebi-global-footer-container/template.php
@@ -1,11 +1,27 @@
 <?php
 
-global $vf_plugin;
-if ( ! $vf_plugin instanceof VF_EBI_Global_Footer) return;
+$acf_id = isset($acf_id) ? $acf_id : false;
 
-$content = $vf_plugin->api_html();
+$id = get_field('vf_ebi_global_footer_node_id', $acf_id);
+$id = intval($id);
+$id = $id ? $id : 6683;
+
+$vars = array(
+  'source'              => 'contenthub',
+  'pattern'             => 'node-body',
+  'filter-content-type' => 'article',
+  'filter-id'           => $id,
+);
+
+// Setup base API URL
+$url = VF_Cache::get_api_url();
+$url .= '/pattern.html';
+$url = add_query_arg($vars, $url);
+
+// Request HTML from the Content Hub API
+$content = VF_Cache::fetch($url);
 $hash = VF_Cache::hash(
-  esc_url_raw($vf_plugin->api_url())
+  esc_url_raw($url)
 );
 
 if (vf_cache_empty($content)) {
--- wp-content/plugins/vf-ebi-global-header-container/index.php
+++ wp-content/plugins/vf-ebi-global-header-container/index.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: VF-WP EBI VF 1.3 Global Header
 Description: VF-WP theme global container.
-Version: 1.0.0-beta.1
+Version: 1.0.0-beta.2
 Author: EMBL-EBI Web Development
 Plugin URI: https://github.com/visual-framework/vf-wp
 Text Domain: vfwp
@@ -24,25 +24,11 @@ class VF_EBI_Global_Header extends VF_Plugin {
     'post_type'  => 'vf_container',
   );
 
-  protected $API = array(
-    'pattern'             => 'node-body',
-    'filter-content-type' => 'article',
-    'source'              => 'contenthub',
-  );
-
   public function __construct(array $params = array()) {
     parent::__construct('vf_ebi_global_header');
     if (array_key_exists('init', $params)) {
       parent::initialize();
     }
-  }
-
-  function api_url(array $query_vars = array()) {
-    $id = intval(get_field('vf_ebi_global_header_node_id', $this->post()->ID));
-    $vars = array(
-      'filter-id' => $id ? $id : 6682
-    );
-    return parent::api_url(array_merge($vars, $query_vars));
   }
 
 } // VF_EBI_Global_Header
