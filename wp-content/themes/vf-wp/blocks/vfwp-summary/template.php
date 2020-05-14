4bd7bad6faa3cb140a5d12ad2cb8459566a01ca9
David Bushell <david@dbushell.com>
2020-05-14 09:08:55 +0100

    vfwp-summary update template logic with admin banners

--- wp-content/themes/vf-wp/blocks/vfwp-summary/template.php
+++ wp-content/themes/vf-wp/blocks/vfwp-summary/template.php
@@ -1,10 +1,34 @@
 <?php
 
+// Block preview in Gutenberg editor
+$is_preview = isset($is_preview) && $is_preview;
+
+$type = get_field('select_type');
 $image = get_field('image');
+
 $title = get_field('title');
+$title = trim($title);
+
 $text = get_field('text', false, false);
-$page_cont = wpautop($text);
-$vf_class = str_replace('<p>', '<p class="vf-summary__text">', $page_cont);
+$text = wpautop($text);
+$text = str_replace('<p>', '<p class="vf-summary__text">', $text);
+
+// Function to output a banner message in the Gutenberg editor only
+$admin_banner = function($message, $modifier = 'info') use ($is_preview) {
+  if ( ! $is_preview) {
+    return;
+  }
+?>
+<div class="vf-banner vf-banner--alert vf-banner--<?php echo $modifier; ?>">
+  <div class="vf-banner__content">
+    <p class="vf-banner__text">
+      <?php echo $message; ?>
+    </p>
+  </div>
+</div>
+<!--/vf-banner-->
+<?php
+};
 
 ?>
 
@@ -14,27 +38,36 @@ $vf_class = str_replace('<p>', '<p class="vf-summary__text">', $page_cont);
   }
 </style>
 
-<?php 
-if ( get_field('select_type') == 'Custom' ) { ?>
+<?php
+if ( $type === 'custom' ) {
+  if (vf_html_empty($title) && vf_html_empty($text)) {
+    $admin_banner(__('Please enter custom content for this summary.', 'vfwp'));
+    return;
+  }
+?>
 <article class="vf-summary vf-summary--has-image">
   <img class="vf-summary__image vf-summary__image--thumbnail" src="<?php echo get_field('image'); ?>">
   <h3 class="vf-summary__title">
     <?php echo esc_html($title); ?>
     </a>
   </h3>
-  <?php echo ($vf_class)?>
+  <?php echo $text; ?>
 </article>
-<?php    
+<?php
+  return;
 }
 
-else if ( get_field('select_type') == 'Post' ) {  
-    $object_post = get_field('post');
-    if ($object_post) {
-        // Setup first post data so template tags work
-        global $post;
-        $old_post = $post;
-        setup_postdata($post = $object_post);
-      ?>
+if ( $type === 'post' ) {
+  $object_post = get_field('post');
+  if ($object_post instanceof WP_Post === false) {
+    $admin_banner(__('Please select a post for this summary.', 'vfwp'));
+    return;
+  }
+  // Setup first post data so template tags work
+  global $post;
+  $old_post = $post;
+  setup_postdata($post = $object_post);
+?>
 <article class="vf-summary vf-summary--news">
   <span class="vf-summary__date"><time title="<?php the_time('c'); ?>"
       datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?></time></span>
@@ -46,20 +79,24 @@ else if ( get_field('select_type') == 'Post' ) {
   </h3>
   <p class="vf-summary__text"><?php echo get_the_excerpt() ?></p>
 </article>
-<?php wp_reset_postdata(); ?>
-<?php 
-} }
+<?php
+  wp_reset_postdata();
+  return;
+}
 
-else if ( get_field('select_type') == 'Event' ) { 
-    $object_event = get_field('event');
-    if ($object_event) {
-        // Setup first post data so template tags work
-        global $post;
-        $old_post = $post;
-        setup_postdata($post = $object_event);
-        $location = get_field('vf_event_location',$post->ID);
-        $event_type = get_field('vf_event_event_type',$post->ID); 
-      ?>
+if ( $type === 'event' ) {
+  $object_event = get_field('event');
+  if ($object_post instanceof WP_Post === false) {
+    $admin_banner(__('Please select an event for this summary.', 'vfwp'));
+    return;
+  }
+  // Setup first post data so template tags work
+  global $post;
+  $old_post = $post;
+  setup_postdata($post = $object_event);
+  $location = get_field('vf_event_location',$post->ID);
+  $event_type = get_field('vf_event_event_type',$post->ID);
+?>
 <a href="<?php the_permalink(); ?>"
   class="vf-summary vf-summary--event | vf-summary--is-link vf-summary--easy vf-summary-theme--primary">
   <p class="vf-summary__date"><time title="<?php the_time('c'); ?>"
@@ -75,7 +112,8 @@ else if ( get_field('select_type') == 'Event' ) {
       d="M23.6 11.289l-9.793-9.7a2.607 2.607 0 00-3.679.075 2.638 2.638 0 00-.068 3.689l3.871 3.714a.25.25 0 01-.173.43H2.135A2.28 2.28 0 00.1 12c0 .815.448 2.51 2 2.51h11.679a.25.25 0 01.177.427l-3.731 3.733a2.66 2.66 0 003.758 3.754l9.625-9.72a1 1 0 00-.008-1.415z"
       fill="" fill-rule="nonzero" /></svg>
 </a>
-<?php wp_reset_postdata(); ?>
-<?php 
-} }
+<?php
+  wp_reset_postdata();
+  return;
+}
 ?>
