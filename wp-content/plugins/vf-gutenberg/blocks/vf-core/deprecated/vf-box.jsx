8d50ff6e5fc4ae72cb2919170cc88ede8f8fa0e3
David Bushell <david@dbushell.com>
2020-05-20 10:54:39 +0100

    deprecate old vf-box React block

--- wp-content/plugins/vf-gutenberg/blocks/vf-core/deprecated/vf-box.jsx
+++ wp-content/plugins/vf-gutenberg/blocks/vf-core/deprecated/vf-box.jsx
@@ -1,16 +1,23 @@
 /**
 Block Name: Box
+
+DEPRECATED
+
+Deprecated in favour of the ACF block.
+Block is hidden from the Gutenberg inserter with `isInsertable` setting.
+
 */
 import React from 'react';
 import {__} from '@wordpress/i18n';
 import template from '@visual-framework/vf-box/vf-box.precompiled';
-import {withTransientStyle} from '../hooks/with-transient';
-import useVFCoreSettings from '../hooks/use-vf-core-settings';
+import {withTransientStyle} from '../../hooks/with-transient';
+import useVFCoreSettings from '../../hooks/use-vf-core-settings';
 
 export default useVFCoreSettings({
   ver: '1.0.2',
   name: 'vf/box',
   title: __('Box'),
+  isInsertable: false,
   attributes: {
     box_heading: {
       type: 'string'
