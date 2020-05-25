d817cd4f82a8364a03b1f39558d274bd59800e1b
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-05-25 13:42:42 +0200

    adds vf-figure widget

---
+++ wp-content/themes/vf-wp/widgets/vfwp-embed/index.php
@@ -0,0 +1,58 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists('VF_Widget_Figure') ) :
+
+class VF_Widget_Figure extends WP_Widget {
+
+  public function __construct() {
+    parent::__construct(
+      'vf_widget_figure',
+      __('Figure', 'vfwp')
+    );
+  }
+
+  /**
+   * Render the plugin using the widget ACF data
+   */
+  public function widget($args, $instance) {
+    
+    $widget_id = 'widget_' . $args['widget_id'];
+    $caption = get_field('caption', $widget_id);
+    $image = get_field('image', $widget_id);
+    
+      $image = wp_get_attachment_image($image['ID'], 'medium', false, array(
+        'class'    => 'vf-figure__image',
+        'loading'  => 'lazy',
+        'itemprop' => 'image',
+      ));
+    
+    ?>
+    
+    <figure class="vf-figure">
+    
+        <?php echo ($image); ?>
+    
+        <figcaption class="vf-figure__caption"><?php echo ($caption); ?></figcaption>
+    
+    </figure>
+    <?php
+  }
+
+  public function form($instance) {
+    // Do nothing...
+  }
+
+} // VF_Widget_Figure
+
+endif;
+
+/**
+ * Register Links List Widget
+ */
+function register_Figure_widget()
+{
+  register_widget( 'VF_Widget_Figure' );
+}
+add_action( 'widgets_init', 'register_figure_widget' ); ?>
