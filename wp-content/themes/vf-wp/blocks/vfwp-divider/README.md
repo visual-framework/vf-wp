ede9f883ed70e12bbc740a145cb944e6f8f5380e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-05-28 14:45:32 +0200

    vf-divider block
    - full width toggle option

---
+++ wp-content/themes/vf-wp/blocks/vfwp-divider/README.md
@@ -0,0 +1,11 @@
+See [VF-WP Blocks](/docs/blocks.md) general documentation.
+
+# Divider (block)
+
+The `vf-divider` Visual Framework pattern.
+
+## Configuration
+
+ACF / Block data:
+
+Block `name`: `acf/vfwp-divider`
--- wp-content/themes/vf-wp/functions.php
+++ wp-content/themes/vf-wp/functions.php
@@ -21,6 +21,8 @@ require_once('blocks/vfwp-page-header/index.php');
 require_once('blocks/vfwp-section-header/index.php');
 require_once('widgets/vfwp-figure/index.php');
 require_once('widgets/vfwp-embed/index.php');
+require_once('blocks/vfwp-divider/index.php');
+
 
 global $vf_admin;
 if ( ! isset($vf_admin)) {
