863f75d173c01df6b97ef3a528710c3a1113170e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-06-02 12:37:23 +0200

    Add favicons to vf-wp parent theme #314

--- wp-content/themes/vf-wp/partials/head.php
+++ wp-content/themes/vf-wp/partials/head.php
@@ -1,6 +1,14 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?> class="vf-no-js">
 <head>
+  <link rel="shortcut icon" href="https://www.embl.org/guidelines/design/assets/embl-favicon/assets/favicon.ico">
+  <link rel="apple-touch-icon" sizes="180x180" href="https://www.embl.org/guidelines/design/assets/embl-favicon/assets/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="https://www.embl.org/guidelines/design/assets/embl-favicon/assets/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="https://www.embl.org/guidelines/design/assets/embl-favicon/assets/favicon-16x16.png">
+  <link rel="manifest" href="https://www.embl.org/guidelines/design/assets/embl-favicon/assets/site.webmanifest">
+  <link rel="mask-icon" href="https://www.embl.org/guidelines/design/assets/embl-favicon/assets/safari-pinned-tab.svg" color="#ffffff">
+  <meta name="msapplication-TileColor" content="#ffffff">
+  <meta name="theme-color" content="#ffffff">
   <meta charset="<?php bloginfo('charset'); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head(); ?>
