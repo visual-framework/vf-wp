4871c848ba92a05def94fff211f02782483fb6c8
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-06-17 13:42:50 +0200

    page template update

--- wp-content/themes/vf-wp-groups/page.php
+++ wp-content/themes/vf-wp-groups/page.php
@@ -9,62 +9,60 @@ global $vf_theme;
 $title = get_the_title();
 ?>
 
-<?php
+<?php 
+
 $layout = get_field('page_layout');
 
-
-  if ($layout == 'full') { 
-    
-
+if ($layout == 'full') { 
+  
 $open_wrap = function($html, $block_name) {
-    $html = '
-  <div class="embl-grid embl-grid--has-centered-content">
-    <div></div>
-    <div>
-  ' . $html;
-  return $html;
+  $html = '
+<div class="embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+' . $html;
+return $html;
 };
 
 $close_wrap = function($html, $block_name) {
-    $html .= '
-    </div>
-    <div></div>
+  $html .= '
   </div>
-  <!--/embl-grid-->';
-  return $html;
+  <div></div>
+</div>
+<!--/embl-grid-->';
+return $html;
 };
 
 add_filter(
-  'vf/__experimental__/theme/content/open_block_wrap',
-  $open_wrap,
-  10, 2
+'vf/__experimental__/theme/content/open_block_wrap',
+$open_wrap,
+10, 2
 );
 
 add_filter(
-  'vf/__experimental__/theme/content/close_block_wrap',
-  $close_wrap,
-  10, 2
+'vf/__experimental__/theme/content/close_block_wrap',
+$close_wrap,
+10, 2
 );
 
 ?>
 
 <?php
-if ( has_block( 'acf/vfwp-intro', $post ) ) {  
 
-parse_blocks( 'acf/vfwp-intro' ); } 
+  if ( has_block( 'acf/vfwp-intro', $post ) ) {  
+  parse_blocks( 'acf/vfwp-intro' ); } 
 
-else if ( has_block( 'acf/vfwp-page-header', $post ) ) {
-
-parse_blocks( 'acf/vfwp-page-header' ); }
+  else if ( has_block( 'acf/vfwp-page-header', $post ) ) {
+  parse_blocks( 'acf/vfwp-page-header' ); }
 
 else { ?>
 
 <section class="embl-grid embl-grid--has-centered-content">
   <div></div>
   <div>
-  <h1 class="vf-text vf-text-heading--1">
-        <?php echo $title;?>
-      </h1>
+    <h1 class="vf-text vf-text-heading--1">
+      <?php echo $title;?>
+    </h1>
   </div>
   <div></div>
 </section>
@@ -74,11 +72,11 @@ else { ?>
 $vf_theme->the_content();
 
 ?>
-  <?php }
-else {
-   ?>
+<?php }
 
-    <section class="vf-inlay">
+else { ?>
+
+  <section class="vf-inlay">
   <div class="vf-inlay__content vf-u-background-color-ui--white">
     <main class="vf-inlay__content--main">
       <h1 class="vf-text vf-text-heading--1">
@@ -99,13 +97,7 @@ else {
   </div>
 </section>
 
-
-
-
-
-
-
-    <?php }
+<?php }
 
 get_footer();
 
