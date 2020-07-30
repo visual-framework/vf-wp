4a7bc3e03dc7a63786c97eaeece57d80aa6b12b1
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-07-30 09:20:19 +0200

    Update vf-summary--document.php

--- wp-content/themes/vf-wp-documents/partials/vf-summary--document.php
+++ wp-content/themes/vf-wp-documents/partials/vf-summary--document.php
@@ -2,48 +2,50 @@
 $types = get_the_terms( $post->ID , 'document_type' );
 $topics = get_the_terms( $post->ID , 'document_topic' );
 
+$image = get_field('image');
+if ( ! is_array($image)) {
+  $image = null;
+} else {
+  $image = wp_get_attachment_image($image['ID'], 'medium', false, array(
+    'class'    => 'vf-summary__image',
+    'style'    => 'width: 175px; height: auto; ',
+    'loading'  => 'lazy',
+    'itemprop' => 'image',
+  ));
+}
 ?>
-<article class="vf-summary">
+<article class="vf-summary vf-summary--news">
+  <time class="vf-summary__date vf-u-margin__bottom--0" style="margin-left: 0;" title="<?php the_time('c'); ?>" datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?></time>
 
-
- <time class="vf-summary__date vf-u-margin__bottom--0" style="margin-left: 0;" title="<?php the_time('c'); ?>"
-      datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?></time>
-
-	         <h4 class="vf-summary__title">
-
-   <a href="<?php the_permalink(); ?>" class="vf-link "
-        ><?php the_title(); ?></a>
-
-	</h4>
-
-	<p class="vf-summary__meta | vf-u-margin__bottom--sm">
-
-  Type:
-<?php
-  if (is_array($types)) {
-		 foreach ( $types as $type ) :
-		?> <a href="<?php echo esc_url( get_term_link( $type->term_id ) ); ?>" title="<?php echo esc_attr( $type->name ); ?>"><?php echo esc_html( $type->name); ?></a>         <?php endforeach; ?>&nbsp;&nbsp;&nbsp;
-    
-    Topic: 
+ <?php if ($image) {
+    echo $image; 
+  }  ?>
+  <h4 class="vf-summary__title">
+    <a href="<?php the_permalink(); ?>" class="vf-summary__link"><?php the_title(); ?></a>
+  </h4>
+  <p class="vf-summary__meta | vf-u-margin__bottom--sm">Type: 
     <?php
-    if (is_array($topics)) {
-		  foreach ( $topics as $topic ) {
-		?> <a href="<?php echo esc_url( get_term_link( $topic->term_id ) ); ?>"
-              title="<?php echo esc_attr( $topic->name ); ?>"><?php echo esc_html( $topic->name); ?></a>
-            <?php } } ?>
-       </p>
-
-	<p class="vf-summary__meta">Languages:&nbsp;
+      if (is_array($types)) {
+        foreach ( $types as $type ) { ?> 
+          <a href="<?php echo esc_url( get_term_link( $type->term_id ) ); ?>" title="<?php echo esc_attr( $type->name ); ?>"><?php echo esc_html( $type->name); ?>
+          </a>
+    <?php } } ?>
+      &nbsp;&nbsp;&nbsp;&nbsp;Topic:
+    <?php
+      if (is_array($topics)) {
+        foreach ( $topics as $topic ) { ?> 
+          <a href="<?php echo esc_url( get_term_link( $topic->term_id ) ); ?>"
+            title="<?php echo esc_attr( $topic->name ); ?>"><?php echo esc_html( $topic->name); ?>
+          </a>
+    <?php } } ?>
+      &nbsp;&nbsp;&nbsp;&nbsp;Language(s):&nbsp;
     <?php if( have_rows('vf-document_file_upload') ): ?>
-
-        <?php while( have_rows('vf-document_file_upload') ): the_row();
-
-      $language = get_sub_field('language');
-      $file = get_sub_field('file');?>
-
+      <?php while( have_rows('vf-document_file_upload') ): the_row();
+        $language = get_sub_field('language');
+        $file = get_sub_field('file');?>
         <a href="<?php echo $file['url']; ?>"><?php echo esc_html($language) ?></a>&nbsp;
-        <?php endwhile; ?>
-    <?php endif; } ?>
-	</p>
-        </article>
+      <?php endwhile; ?>
+    <?php endif; ?>
+  </p>
+</article>
 <!--/vf-summary-->
