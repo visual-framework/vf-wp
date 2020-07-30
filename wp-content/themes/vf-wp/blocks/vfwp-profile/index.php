12ddcd46eb3a2c242cd7eef7dc700d030a33a660
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-07-30 11:26:26 +0200

    registers vf-profile block

---
+++ wp-content/themes/vf-wp/blocks/vfwp-profile/index.php
@@ -0,0 +1,43 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists('VFWP_Profile') ) :
+
+class VFWP_Profile extends VFWP_Block {
+
+  public function __construct() {
+
+    parent::__construct(__FILE__);
+  }
+
+  /**
+   * Return the block name
+   */
+  static public function get_name() {
+    return 'vfwp-profile';
+  }
+
+  /**
+   * Return Gutenberg block registration configuration
+   * https://www.advancedcustomfields.com/resources/acf_register_block_type/
+   * https://developer.wordpress.org/block-editor/developers/block-api/block-registration/
+   */
+  public function get_config() {
+    return array(
+      'name'     => $this->get_name(),
+      'title'    => 'Profile',
+      'category' => 'vf/wp',
+      'supports' => array(
+        'align'           => false,
+        'customClassName' => false
+      )
+    );
+  }
+
+} // VFWP_Profile
+
+// Initialize one instance
+$vfwp_profile = new VFWP_Profile();
+
+endif; ?>
