734c36695fff96f404d68c368ebfd0788aabe98d
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-08-05 12:20:26 +0200

    includes partials

--- wp-content/plugins/vf-events/templates/single-vf_event.php
+++ wp-content/plugins/vf-events/templates/single-vf_event.php
@@ -4,117 +4,72 @@ get_header();
 
 global $post;
 
+$title = get_the_title($post->post_parent);
+
 $start_date = get_field(
-  'vf_event_start_date',
-  $post->ID
+  'vf_event_start_date', $post->post_parent
 );
 $start = DateTime::createFromFormat('j M Y', $start_date);
 
 $end_date = get_field(
-  'vf_event_end_date',
-  $post->ID
+  'vf_event_end_date', $post->post_parent
 );
 $end = DateTime::createFromFormat('j M Y', $end_date);
 
-
 $location = get_field(
-  'vf_event_location',
-  $post->ID
+  'vf_event_location', $post->post_parent
 );
 
 $location = strtoupper($location);
 
 $submission_closing = get_field(
-  'vf_event_submission_closing',
-  $post->ID
+  'vf_event_submission_closing', $post->post_parent
 );
 
 $registration_closing = get_field(
-  'vf_event_registration_closing',
-  $post->ID
+  'vf_event_registration_closing', $post->post_parent
 );
 
 $summary = get_field(
-  'vf_event_summary',
-  $post->ID
+  'vf_event_summary'
 );
 
 $additional_info = get_field(
-  'vf_event_additional_info',
-  $post->ID
+  'vf_event_additional_info'
 );
 
 $event_type = get_field(
-  'vf_event_event_type',
-  $post->ID
+  'vf_event_event_type', $post->post_parent
 );
 
 $registration_link = get_field(
-  'vf_event_registration_link',
-  $post->ID
+  'vf_event_registration_link', $post->post_parent
 );
 
-$image = get_field('vf_event_logo');
-$image = wp_get_attachment_image($image['ID'], 'medium', false, array(
+$logo_image = get_field('vf_event_logo', $post->post_parent);
+$logo_image = wp_get_attachment_image($logo_image['ID'], 'medium', false, array(
     'style'    => 'max-height: 95px; width: auto;',
     'loading'  => 'lazy',
     'itemprop' => 'image',
   ));
 
+$hero_image = get_field('vf_event_hero', $post->post_parent);
+$hero_image = wp_get_attachment_url($hero_image['ID'], 'medium', false, array(
+    'loading'  => 'lazy',
+    'itemprop' => 'image',
+  ));
+
+$info_box = get_field('vf_event_info_box', $post->post_parent);
+$theme = get_field('vf_event_theme', $post->post_parent);
+$cpp_container = get_field('vf_event_cpp_container', $post->post_parent);
+$box_items = get_field('vf_event_box_items', $post->post_parent);
+$accommodation = get_field('accommodation');
 ?>
 
-<section class="vf-hero vf-hero--inlay vf-hero--hard  | vf-hero-theme--primary | vf-u-margin__bottom--xxl">
-    <style>
-  .vf-hero {
-    --vf-hero-bg-image: url('https://acxngcvroo.cloudimg.io/v7/https://www.embl.org/files/wp-content/uploads/vf-hero-intense.png');
-    --vf-hero-grid__row--initial: 384px;
-  }
-
-  </style>
-        <div class="vf-hero__content">
-    <h2 class="vf-hero__heading">
-      EMBL <?php echo esc_html($event_type); ?>   </h2>
-    <p class="vf-hero__text" style="font-size: 32px;">
-    <?php the_title(); ?>
-    </p>
-  </div>
-</section>
-
-<section class="vf-grid vf-grid__col-3">
-  <div class="vf-u-padding--md | vf-u-padding__right--0">
-    <p class="vf-text-body vf-text-body--2">
-
-      <?php 
-        if ($end_date) { 
-          echo $start->format('j M'); ?> - <?php echo $end->format('j M Y'); ?> 
-      <?php } 
-        else {
-          echo $start->format('j M Y'); 
-        }
-      ?>
-      &nbsp;&nbsp;&nbsp; <?php echo esc_html($location); ?></p>
-
-    <p class="vf-text-body vf-text-body--4" style="font-weight: 400;">Application deadline: <?php echo esc_html($registration_closing); ?></p>
-
-    <?php if ( ! empty($submission_closing)) { ?>
-    <p class="vf-text-body vf-text-body--4" style="font-weight: 400;">Abstract submission deadline: <?php echo esc_html($submission_closing); ?></p>
-    <?php } ?>
-
-  </div>
-
-  <div class="vf-u-padding__top--md">
-    <?php if ( ! empty($registration_link)) { ?>
-      <a href="<?php echo esc_url($registration_link); ?>"><button class="vf-button vf-button--primary vf-button--sm">Register</button></a>
-    <?php } ?>
-  </div>
-  <div>
-    <figure class="vf-figure vf-figure--align vf-figure--align-centered">
-    <?php echo($image); ?> 
-    </figure>
-  </div>
-</section>
-
-<hr class="vf-divider">
+<?php 
+// vf-hero container
+include( plugin_dir_path( __FILE__ ) . 'partials/hero.php'); 
+?>
 
 <section class="vf-grid vf-grid__col-3">
   <div class="vf-grid__col--span-2 | vf-content">
@@ -127,39 +82,18 @@ $image = wp_get_attachment_image($image['ID'], 'medium', false, array(
       }
       ?>
     </div>
-    <div>
-      <div class="vf-box vf-box--normal vf-box-theme--quinary">
-        <h3 class="vf-box__heading">Information for participants</h3>
-        <a class= "vf-link" href="#"><p class="vf-box__text">Financial Assistance</p></a>
-        <p class="vf-box__text">Find out more about the Registration Fee Waivers and Travel Grants available for EMBL Courses and Conferences in Heidelberg</p>
-        
-        <a class= "vf-link" href="#"><p class="vf-box__text">Childcare</p></a>
-        <p class="vf-box__text">EMBL offers onsite childcare for many of the conferences and symposia run at EMBL Heidelberg.</p>
-        
-        <a class= "vf-link" href="#"><p class="vf-box__text">Accommodation</p></a>
-        <p class="vf-box__text">For all courses and conferences at EMBL the Course and Conference Office blocks accommodation in advance to secure rooms for their guests.</p>
-        
-        <a class= "vf-link" href="#"><p class="vf-box__text">FAQ</p></a>
-        <p class="vf-box__text">We aim to provide you with all the necessary and relevant information in order to make your participation at one of our events as enjoyable as possible. </p>
-        
-        <a class= "vf-link" href="#"><p class="vf-box__text">Travel information</p></a>
-        <p class="vf-box__text">During conferences EMBL arranges frequent bus transfers between EMBL and various stops around Heidelberg.</p>
-        
-        <a class= "vf-link" href="#"><p class="vf-box__text">Terms and Conditions</p></a>
-        <p class="vf-box__text">Don't forget to read the small print associated with your course or conference registration.</p>
-        
-        <a class= "vf-link" href="#"><p class="vf-box__text">Contact</p></a>
-        <p class="vf-box__text">All the details you need to get in touch with the EMBL Course and Conference Office.</p>
-      </div>
-    </div>
-  </section>
+
+<?php 
+// info box for EMBL CCO
+include( plugin_dir_path( __FILE__ ) . 'partials/cco-info.php'); ?>
   
-  <section class="vf-content">
+</section>
+
+<section class="vf-content">
     <?php 
       if ($summary) {
         the_content();
-      }
-      ?>
+      } ?>
     
     <?php if ( ! empty($additional_info)) { ?>
         <h2>
@@ -168,77 +102,12 @@ $image = wp_get_attachment_image($image['ID'], 'medium', false, array(
         <p><?php echo ($additional_info); ?></p>
     <?php } ?>    
 </section>
-<section class="vf-content">
-  <p>EMBL Courses and Conferences are kindly supported by our <a href="https://www.embl.de/aboutus/support-embl/corporate-partnership-programme/index.html">Corporate Partnership Programme</a></p>
-  <p><b>Founder partners</b></p>
-  <div class="vf-grid vf-grid__col-6">
-      <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/leica-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/olympus-logo.jpg" style="width: 120px">
-    </figure>
-  </div>
-  <p><b>Corporate partners</b></p>
-  <div class="vf-grid vf-grid__col-6">
-      <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/10x-Genomics_120x82.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/bd-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Bio-Rad.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/boehringer-ingelheim-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/eppendorf-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/gsk-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Milteniy-Biotec_logo120.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/NetApp_logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Stilla-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/thermofisher-logo.jpg" style="width: 120px">
-    </figure>
-  </div>
-  <p><b>Associate partners</b></p>
-  <div class="vf-grid vf-grid__col-6">
-      <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/merck-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/NEB_logo_120.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/nikon-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Promega_logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Roche_logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/sanofi-logo.jpg" style="width: 120px">
-    </figure>
-    <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Sartorius.jpg" style="width: 120px">
-    </figure>
-  </div>    
-</section>
 
-<?php
+<?php 
+// CPP container
+include( plugin_dir_path( __FILE__ ) . 'partials/cpp-container.php'); ?>
+
+<?php 
 
 get_footer();
 
