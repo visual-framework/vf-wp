2f0612639ca3f3a94632ca540799a9c990f6ba96
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-08-11 07:44:31 +0200

    moves to partials

---
+++ wp-content/themes/vf-wp-intranet/partials/popular-container.php
@@ -0,0 +1,106 @@
+<section class="embl-grid | vf-content">
+  <div class="vf-section-header">
+    <p class="vf-section-header__heading">Popular </p>
+    <p class="vf-section-header__text"></p>
+  </div>
+  <div>
+  <div class="vf-tabs">
+    <ul class="vf-tabs__list" data-vf-js-tabs>
+        <li class="vf-tabs__item">
+            <a class="vf-tabs__link" href="#vf-tabs__section--1">EMBL Barcelona</a>
+        </li>
+        <li class="vf-tabs__item">
+            <a class="vf-tabs__link" href="#vf-tabs__section--2">EMBL Grenoble</a>
+        </li>
+        <li class="vf-tabs__item">
+            <a class="vf-tabs__link" href="#vf-tabs__section--3">EMBL Hamburg</a>
+        </li>
+        <li class="vf-tabs__item">
+            <a class="vf-tabs__link" href="#vf-tabs__section--4">EMBL Heidelberg</a>
+        </li>
+        <li class="vf-tabs__item">
+            <a class="vf-tabs__link" href="#vf-tabs__section--5">EMBL Rome</a>
+        </li>
+    </ul>
+</div>
+
+<div class="vf-tabs-content" data-vf-js-tabs-content>
+    <section class="vf-tabs__section" id="vf-tabs__section--1">
+        <div class="vf-grid vf-grid__col-3">
+          <div class="vf-links vf-u-margin__top--xl">
+            <ul class="vf-links__list | vf-list">
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                Lunch and food options
+                </a>
+              </li>
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                  Room reservations
+                </a>
+              </li>
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                Accommodation
+                </a>
+              </li>
+          </ul>
+          </div>
+          <div class="vf-links vf-u-margin__top--xl">
+            <ul class="vf-links__list | vf-list">
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                Lunch and food options
+                </a>
+              </li>
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                  Room reservations
+                </a>
+              </li>
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                Accommodation
+                </a>
+              </li>
+          </ul>
+          </div>
+          <div class="vf-links vf-u-margin__top--xl">
+            <ul class="vf-links__list | vf-list">
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                Lunch and food options
+                </a>
+              </li>
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                  Room reservations
+                </a>
+              </li>
+              <li class="vf-list__item">
+                <a class="vf-list__link" href="JavaScript:Void(0);">
+                Accommodation
+                </a>
+              </li>
+          </ul>
+          </div>
+        </div>
+    </section>
+    <section class="vf-tabs__section" id="vf-tabs__section--2">
+        <h2>Section 2</h2>
+        <p>Nullam at diam nec arcu suscipit auctor non a erat. Sed et magna semper, eleifend magna non, facilisis nisl. Proin et est et lorem dictum finibus ut nec turpis. Aenean nisi tortor, euismod a mauris a, mattis scelerisque tortor. Sed dolor risus, varius a nibh id, condimentum lacinia est. In lacinia cursus odio a aliquam. Curabitur tortor magna, laoreet ut rhoncus at, sodales consequat tellus.</p>
+    </section>
+    <section class="vf-tabs__section" id="vf-tabs__section--3">
+        <h2>Section 3</h2>
+        <p>Phasellus ac tristique orci. Nulla maximus <a class="vf-link" href="#">justo nec dignissim consequat</a>. Sed vehicula diam sit amet mi efficitur vehicula in in nisl. Aliquam erat volutpat. Suspendisse lorem turpis, accumsan consequat consectetur gravida, <a class="vf-link" href="#">pellentesque ac ante</a>. Aliquam in commodo ligula, sit amet mollis neque. Vestibulum at facilisis massa.</p>
+    </section>
+    <section class="vf-tabs__section" id="vf-tabs__section--4">
+        <h2>Section 4</h2>
+    </section>
+    <section class="vf-tabs__section" id="vf-tabs__section--5">
+        <h2>Section 5</h2>
+    </section>
+
+</div>
+</div>
+</section>
