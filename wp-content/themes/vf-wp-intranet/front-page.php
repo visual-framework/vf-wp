b9cfcbefcb658eaaad4cecf9c47a6fefe8d7a986
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-08-11 07:45:19 +0200

    inculdes partials on the front page

--- wp-content/themes/vf-wp-intranet/front-page.php
+++ wp-content/themes/vf-wp-intranet/front-page.php
@@ -46,6 +46,13 @@ $close_wrap,
 );
 
 ?>
+
+<?php include(locate_template('partials/explore-container.php', false, false)); ?>
+
+<?php include(locate_template('partials/popular-container.php', false, false)); ?>
+
+<hr class="vf-divider">
+
 <section class="vf-summary-container | embl-grid">
   <div class="vf-section-header">
     <a href="https://www.embl.org/news/" class="vf-section-header__heading vf-section-header__heading--is-link">INsites <svg class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path></svg></a>
@@ -63,8 +70,32 @@ $close_wrap,
 
     </div>
   </div>
+</section> 
+<hr class="vf-divider">
+<section class="vf-summary-container | embl-grid">
+  <div class="vf-section-header">
+    <a href="https://www.embl.org/news/" class="vf-section-header__heading vf-section-header__heading--is-link">Events <svg class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path></svg></a>
+    <p class="vf-section-header__text">EMBL's internal events</p>
+  </div>
 
-</section>  
+    <div>
+    <?php
+			$eventsLoop = new WP_Query (array('posts_per_page' => 2, 'post_type' => 'vf_event'));
+      while ($eventsLoop->have_posts()) : $eventsLoop->the_post();?>
+      <?php include(locate_template('partials/vf-summary-events.php', false, false)); ?>
+      <?php endwhile;?>
+      <?php wp_reset_postdata(); ?>
+    </div>
+</section> 
+
+<?php include(locate_template('partials/community-blog-container.php', false, false)); ?>
+
+
+
+
+
+
+
 <?php 
 
 $vf_theme->the_content();
