86b7db8f2ae57e5bfe1898bf3808472df892eca9
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-08-12 11:11:51 +0200

    updates template for insites

--- wp-content/themes/vf-wp-intranet/single.php
+++ wp-content/themes/vf-wp-intranet/single.php
@@ -14,29 +14,21 @@ the_post();
 ?>
 
 <section class="embl-grid embl-grid--has-centered-content | vf-u-background-color-ui--white | vf-u-padding__top--xxl | vf-u-margin__bottom--0">
-  <div class="article-left-col">
-    <div class="vf-article-meta-information">
-      <div class="vf-author | vf-article-meta-info__author">
-        <p class="vf-author__name">
-          <a class="vf-link"
-            href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
-        </p>
-        <a class="vf-author--avatar__link | vf-link"
-          href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>">
-          <?php echo get_avatar( get_the_author_meta( 'ID' ), 48, '', '', array('class' => 'vf-author--avatar')); ?>
-        </a>
-      </div>
-      <div class="vf-meta__details">
-        <p class="vf-meta__date"><time title="<?php the_time('c'); ?>" datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?></time></p>
-      </div>
-    </div>
+  <div>
   </div>
 
   <div class="vf-content | vf-u-padding__bottom--xxl">
+    <p class="vf-summary__date"><time title="<?php the_time('c'); ?>" datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?></time></p>
     <h1><?php the_title(); ?></h1>
     <p class="vf-lede | vf-u-padding__top--md | vf-u-padding__bottom--xxl">
       <?php echo get_post_meta($post->ID, 'article_intro', true); ?>
     </p>
+    <figure class="vf-figure | vf-u-float__right--md | vf-u-padding__left--xl">
+      <?php the_post_thumbnail('full', array('class' => 'vf-figure__image', 'style' => 'width: 300px;')); ?>
+      <figcaption class="vf-figure__caption">
+        <?php echo wp_kses_post(get_post(get_post_thumbnail_id())->post_excerpt); ?>
+      </figcaption>
+    </figure>
 
     <?php the_content(); ?>
 
