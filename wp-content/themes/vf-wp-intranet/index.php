f888e974a622a6c74b95f6bf9266f1d850dea6b3
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-08-12 13:55:25 +0200

    updates posts archive template

--- wp-content/themes/vf-wp-intranet/index.php
+++ wp-content/themes/vf-wp-intranet/index.php
@@ -7,28 +7,21 @@ global $vf_theme;
 $title = $vf_theme->get_title();
 
 ?>
-<section class="vf-inlay">
-  <div class="vf-inlay__content vf-u-background-color-ui--white">
-    <main class="vf-inlay__content--main">
-      <h1 class="vf-text vf-text-heading--1">
-        <?php echo esc_html($title); ?>
-      </h1>
-      <?php
-      while (have_posts()) {
-        the_post();
-        get_template_part('partials/vf-summary--article');
-        if ( ! $vf_theme->is_last_post()) {
-          echo '<hr class="vf-divider">';
-        }
-      }
-      vf_pagination();
-      ?>
-    </main>
-    <?php if (is_active_sidebar('sidebar-blog')) { ?>
-    <aside class="vf-inlay__content--additional">
-      <?php vf_sidebar('sidebar-blog'); ?>
-    </aside>
-    <?php } ?>
+<section class="vf-grid vf-grid__col-3">
+  <div class="vf-grid__col--span-2">
+    <h1 class="vf-text vf-text-heading--1">
+      <?php echo esc_html($title); ?>
+    </h1>
+    <?php
+    while (have_posts()) {
+      the_post();
+      get_template_part('partials/vf-summary--article');
+    }
+    vf_pagination();
+    ?>
+  </div>
+  <div>
+
   </div>
 </section>
 <?php
