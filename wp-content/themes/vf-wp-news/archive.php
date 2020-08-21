d38ee3a5e09074ffb71b5b2a68d11f334b36614d
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-08-21 13:15:46 +0200

    removes vf-inlay layout

--- wp-content/themes/vf-wp-news/archive.php
+++ wp-content/themes/vf-wp-news/archive.php
@@ -26,24 +26,23 @@ if (is_search()) {
 }
 
 ?>
-<section class="vf-inlay | vf-u-margin__bottom--sm ">
-  <div class="vf-inlay__content vf-u-background-color-ui--off-white | vf-u-margin__bottom--sm | vf-u-padding__top--md">
-    <main class="vf-inlay__content--full-width | vf-u-margin__bottom--0">
+<section class="vf-u-margin__bottom--sm">
+  <div class=" vf-u-background-color-ui--off-white | vf-u-padding--md">
+    <div>
       <div>
         <h1 class="vf-text vf-text-heading--1 | vf-u-margin__bottom--xl" style="font-weight: 400;">
         <?php single_tag_title(); ?></h1>
       </div>
       <div class="vf-grid vf-grid__col-3">
-        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();
-if ( $post->ID == $do_not_duplicate ) continue; ?>
+        <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
         <?php include(locate_template('partials/vf-card--article.php', false, false)); ?>
         <?php endwhile; endif; ?>
       </div>
       <div class="vf-grid" style="margin: 4%"> <?php vf_pagination();
       ?>
       </div>
-    </main>
   </div>
+</section>
 
   <?php include(locate_template('partials/archive-container.php', false, false)); ?>
 
@@ -51,6 +50,5 @@ if ( $post->ID == $do_not_duplicate ) continue; ?>
 
   <?php include(locate_template('partials/newsletter-container.php', false, false)); ?>
 
-</section>
 
 <?php get_footer(); ?>
