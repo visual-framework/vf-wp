c14fbd4f0edd09987b9d9e76542e4bb011617a0e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-09-02 14:44:23 +0200

    replaces the search form

--- wp-content/themes/vf-wp/blocks/vfwp-search/template.php
+++ wp-content/themes/vf-wp/blocks/vfwp-search/template.php
@@ -1,9 +1,4 @@
-<form role="search" method="get" class="vf-form  | vf-search vf-search--inline" action="<?php echo esc_url(home_url('/')); ?>">
-  <div class="vf-form__item | vf-search__item">
-    <input type="search" class="vf-form__input | vf-search__input" value="<?php echo esc_attr(get_search_query()); ?>" name="s">
-  </div>
-  <input type="submit" class="vf-search__button | vf-button vf-button--primary" value="<?php esc_attr_e('Search', 'vfwp'); ?>" placeholder="Enter your search terms">
-</form>
+<?php get_search_form(); ?>
 
 
 
