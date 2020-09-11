3f6d3fccd44174f63ac1ed8634c9b6557072ae78
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-09-11 11:36:19 +0200

    adds vf-nearest-location to the vf-wp-intranet tabs

--- wp-content/themes/vf-wp/assets/scripts/scripts.js
+++ wp-content/themes/vf-wp/assets/scripts/scripts.js
@@ -2055,6 +2055,44 @@ function emblBreadcrumbs() {
 })([Element.prototype, Document.prototype, DocumentFragment.prototype]); // Run it on default
 // emblBreadcrumbs();
 
+
+// Configure an object of your locations to detect
+function vfLocationNearest() {
+
+  let vfLocationNearestLocations = {
+    default: {
+      name: "Heidelberg (default)",
+      latlon: "49.40768, 8.69079"
+    },
+    barcelona: {
+      name: "Barcelona",
+      latlon: "41.38879, 2.15899"
+    },
+    grenoble: {
+      name: "Grenoble",
+      latlon: "45.16667, 5.71667"
+    },
+    hamburg: {
+      name: "Hamburg",
+      latlon: "53.57532, 10.01534"
+    },
+    heidelberg: {
+      name: "Heidelberg",
+      latlon: "49.40768, 8.69079"
+    },
+    hinxton: {
+      name: "EMBL-EBI Hinxton",
+      latlon: "52.2, 0.11667"
+    },
+    rome: {
+      name: "Rome",
+      latlon: "41.89193, 12.51133"
+    }
+  }
+  // Bootstrap location detection
+  vfLocationNearest(vfLocationNearestLocations);
+};
+
 /*
  *
  * scripts.css
@@ -2072,3 +2110,4 @@ vfFormFloatLabels();
 emblContentHub();
 emblBreadcrumbs(); // if you use embl-content-hub-loader, it will automatically invoke emblNotifications
 // emblNotifications();
+vfLocationNearest();
