1b2a2ce1df50c4479db3158d4ce7c7ffb6b7e8e6
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-09-23 10:09:45 +0200

    updates llab and visit templates

--- wp-content/themes/vf-wp-ells/llab-template.php
+++ wp-content/themes/vf-wp-ells/llab-template.php
@@ -12,7 +12,7 @@ $end_date = get_field('labs_end_date');
 $end = DateTime::createFromFormat('j F Y', $end_date);
 
 $application_deadline = get_field('labs_application_deadline');
-$number_of_spots = get_field('labs_number_of_spots');
+$topic_area = get_field('labs_topic_area');
 $download = get_field('labs_download');
 $image = get_field('labs_image');
 
@@ -93,63 +93,39 @@ if ( ! is_array($image)) {
       </div>
     </article>
     <div>
+      <?php if ($application_deadline) { ?>  
       <p class="vf-text-body vf-text-body--3" style="font-weight: 400;"> <span class="vf-badge">
-          Application deadline:</span>&nbsp;<span
-          class="vf-u-text-color--grey"><?php echo ($application_deadline); ?></span></p>
-      <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Contact:<span class="vf-u-text-color--grey">
-          <?php
-            if( have_rows('labs_contact') ):
-            while( have_rows('labs_contact') ) : the_row();
+            Application deadline:</span>&nbsp;<span
+            class="vf-u-text-color--grey"><?php echo ($application_deadline); ?></span></p>
+      <?php } ?>    
+      <?php if ($topic_area) { ?>    
+      <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Topic area:&nbsp;<span
+            class="vf-u-text-color--grey"><?php echo ($topic_area); ?></span></p>
+      <?php } ?>          
+      <?php
+            if( have_rows('labs_contact') ):?>
+                <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Contact:<span class="vf-u-text-color--grey">
+            <?php while( have_rows('labs_contact') ) : the_row();
                 $emails = get_sub_field('labs_emails');
                 echo ($emails);
             endwhile;
-            else :
-            endif; ?>
+            else : ?>
+            </span></p>
+            <?php endif; ?>
 
-        </span></p>
-      <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Application with / without selection</p>
-      <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Number of spots in the LLAB:<span
-          class="vf-u-text-color--grey">&nbsp;<?php echo ($number_of_spots); ?></span></p>
-      <hr class="vf-divider | vf-u-background-color--green--dark">
-      <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Organisers:<span class="vf-u-text-color--grey">
-          <?php
-            if( have_rows('labs_organisers') ):
-            while( have_rows('labs_organisers') ) : the_row();
+            <?php
+            if( have_rows('labs_organisers') ): ?>
+                <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Organisers:<span class="vf-u-text-color--grey">
+            <?php while( have_rows('labs_organisers') ) : the_row();
                 $organisers = get_sub_field('labs_person', false, false);
                 echo ($organisers);
             endwhile;
-        else :
-        endif; ?>
-
+        else : ?>
         </span></p>
-      <hr class="vf-divider | vf-u-background-color--green--dark">
+       <?php endif; ?>
+
       <p class="vf-text-body vf-text-body--3" style="font-weight: 400;">Share:</p>
-      <svg aria-hidden="true" display="none" class="vf-icon-collection vf-icon-collection--social">
-        <defs>
-          <g id="vf-social--linkedin">
-            <rect xmlns="http://www.w3.org/2000/svg" width="5" height="14" x="2" y="8.5" rx=".5" ry=".5" />
-            <ellipse xmlns="http://www.w3.org/2000/svg" cx="4.48" cy="4" rx="2.48" ry="2.5" />
-            <path xmlns="http://www.w3.org/2000/svg"
-              d="M18.5,22.5h3A.5.5,0,0,0,22,22V13.6C22,9.83,19.87,8,16.89,8a4.21,4.21,0,0,0-3.17,1.27A.41.41,0,0,1,13,9a.5.5,0,0,0-.5-.5h-3A.5.5,0,0,0,9,9V22a.5.5,0,0,0,.5.5h3A.5.5,0,0,0,13,22V14.5a2.5,2.5,0,0,1,5,0V22A.5.5,0,0,0,18.5,22.5Z" />
-          </g>
-          <g id="vf-social--facebook">
-            <path xmlns="http://www.w3.org/2000/svg"
-              d="m18.14 7.17a.5.5 0 0 0 -.37-.17h-3.77v-1.41c0-.28.06-.6.51-.6h3a.44.44 0 0 0 .35-.15.5.5 0 0 0 .14-.34v-4a.5.5 0 0 0 -.5-.5h-4.33c-4.8 0-5.17 4.1-5.17 5.35v1.65h-2.5a.5.5 0 0 0 -.5.5v4a.5.5 0 0 0 .5.5h2.5v11.5a.5.5 0 0 0 .5.5h5a.5.5 0 0 0 .5-.5v-11.5h3.35a.5.5 0 0 0 .5-.45l.42-4a.5.5 0 0 0 -.13-.38z" />
-          </g>
-          <g id="vf-social--twitter">
-            <path xmlns="http://www.w3.org/2000/svg"
-              d="M23.32,6.44a.5.5,0,0,0-.2-.87l-.79-.2A.5.5,0,0,1,22,4.67l.44-.89a.5.5,0,0,0-.58-.7l-2,.56a.5.5,0,0,1-.44-.08,5,5,0,0,0-3-1,5,5,0,0,0-5,5v.36a.25.25,0,0,1-.22.25c-2.81.33-5.5-1.1-8.4-4.44a.51.51,0,0,0-.51-.15A.5.5,0,0,0,2,4a7.58,7.58,0,0,0,.46,4.92.25.25,0,0,1-.26.36L1.08,9.06a.5.5,0,0,0-.57.59,5.15,5.15,0,0,0,2.37,3.78.25.25,0,0,1,0,.45l-.53.21a.5.5,0,0,0-.26.69,4.36,4.36,0,0,0,3.2,2.48.25.25,0,0,1,0,.47A10.94,10.94,0,0,1,1,18.56a.5.5,0,0,0-.2,1,20.06,20.06,0,0,0,8.14,1.93,12.58,12.58,0,0,0,7-2A12.5,12.5,0,0,0,21.5,9.06V8.19a.5.5,0,0,1,.18-.38Z" />
-          </g>
-          <g id="vf-social--youtube">
-            <path xmlns="http://www.w3.org/2000/svg"
-              d="M20.06,3.5H3.94A3.94,3.94,0,0,0,0,7.44v9.12A3.94,3.94,0,0,0,3.94,20.5H20.06A3.94,3.94,0,0,0,24,16.56V7.44A3.94,3.94,0,0,0,20.06,3.5ZM16.54,12,9.77,16.36A.5.5,0,0,1,9,15.94V7.28a.5.5,0,0,1,.77-.42l6.77,4.33a.5.5,0,0,1,0,.84Z" />
-          </g>
-          <g id="vf-social--instagram">
-            <path xmlns="http://www.w3.org/2000/svg"
-              d="M17.5,0H6.5A6.51,6.51,0,0,0,0,6.5v11A6.51,6.51,0,0,0,6.5,24h11A6.51,6.51,0,0,0,24,17.5V6.5A6.51,6.51,0,0,0,17.5,0ZM12,17.5A5.5,5.5,0,1,1,17.5,12,5.5,5.5,0,0,1,12,17.5Zm6.5-11A1.5,1.5,0,1,1,20,5,1.5,1.5,0,0,1,18.5,6.5Z" />
-          </g>
-        </defs>
-      </svg>
+      <?php include(locate_template('partials/social-icons.php', false, false)); ?>
       <div class="vf-social-links">
         <ul class="vf-social-links__list">
           <li class="vf-social-links__item">
@@ -190,7 +166,6 @@ if ( ! is_array($image)) {
           </li>
         </ul>
       </div>
-      <hr class="vf-divider | vf-u-background-color--green--dark">
       <?php
         if( $download ): ?>
       <p><a class="vf-link" href="<?php echo $download['url']; ?>">Download</a></p>
