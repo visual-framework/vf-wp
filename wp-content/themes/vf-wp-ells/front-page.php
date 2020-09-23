8f6041339fb2e93a3193ab40b31704d6954707fd
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-09-23 10:09:06 +0200

    fixes double content on the front page

--- wp-content/themes/vf-wp-ells/front-page.php
+++ wp-content/themes/vf-wp-ells/front-page.php
@@ -5,15 +5,7 @@ get_header();
 global $vf_theme;
 
 ?>
-
-<?php the_content(); ?>
-
-<section class="embl-grid embl-grid--has-centered-content">
- <div>
- </div> 
-  <?php the_content(); ?>   
- </div>
-</section>
+<?php the_content(); ?>   
 <?php include(locate_template('partials/ells-footer.php', false, false)); ?>
 
 <?php
