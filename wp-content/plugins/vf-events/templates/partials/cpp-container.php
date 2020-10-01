57a61a8f7e6caeffb4bc3801d993a4509cbe89ec
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-10-01 09:16:15 +0200

    updates cpp logos

--- wp-content/plugins/vf-events/templates/partials/cpp-container.php
+++ wp-content/plugins/vf-events/templates/partials/cpp-container.php
@@ -4,6 +4,7 @@ $cpp_container = get_field('vf_event_cpp_container', $post->post_parent);
 
 ?>
 <?php if ($cpp_container == 1) { ?>
+<hr class="vf-divider | vf-u-margin__top--xxl">
 <section class="vf-content | vf-u-margin__top--xxl">
   <p>EMBL Courses and Conferences are kindly supported by our <a href="https://www.embl.de/aboutus/support-embl/corporate-partnership-programme/index.html">Corporate Partnership Programme</a></p>
   <p><b>Founder partners</b></p>
@@ -18,13 +19,13 @@ $cpp_container = get_field('vf_event_cpp_container', $post->post_parent);
   <p><b>Corporate partners</b></p>
   <div class="vf-grid vf-grid__col-6">
       <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/10x-Genomics_120x82.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/10x-Genomics-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/bd-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Bio-Rad.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Bio-Rad-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/boehringer-ingelheim-logo.jpg" style="width: 120px">
@@ -36,10 +37,10 @@ $cpp_container = get_field('vf_event_cpp_container', $post->post_parent);
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/gsk-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Milteniy-Biotec_logo120.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Milteniy-Biotec-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/NetApp_logo.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/NetApp-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Stilla-logo.jpg" style="width: 120px">
@@ -54,22 +55,22 @@ $cpp_container = get_field('vf_event_cpp_container', $post->post_parent);
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/merck-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/NEB_logo_120.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/neb-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/nikon-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Promega_logo.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Promega-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Roche_logo.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Roche-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
       <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/sanofi-logo.jpg" style="width: 120px">
     </figure>
     <figure class="vf-figure">
-      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Sartorius.jpg" style="width: 120px">
+      <img class="vf-figure__image | vf-u-margin__bottom--0" src="https://www.embl.de/layout/images/cco/3rd_column_text/cpp-logos/Sartorius-logo.jpg" style="width: 120px">
     </figure>
   </div>    
 </section>
