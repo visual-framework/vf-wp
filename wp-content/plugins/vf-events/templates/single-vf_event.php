7c3b8bb46a93c9272f3da032ffa58f16d64d124b
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-10-09 11:07:26 +0200

    removes unused fields

--- wp-content/plugins/vf-events/templates/single-vf_event.php
+++ wp-content/plugins/vf-events/templates/single-vf_event.php
@@ -11,9 +11,6 @@ global $post;
 $summary = get_field('vf_event_summary');
 
 $additional_info = get_field('vf_event_additional_info');
-
-$navigation_bar = get_field('vf_event_navigation_bar');
-$navigation = get_field('vf_event_navigation');
 ?>
 
 <style>
@@ -30,34 +27,13 @@ include( plugin_dir_path( __FILE__ ) . 'partials/hero.php');
 
 <section class="vf-grid vf-grid__col-4">
   <div class="vf-grid__col--span-3 | vf-content">
-    <?php 
-      if ( ! empty($summary)) { 
-        echo ($summary);
-       } 
-      else {
-        the_content();
-      }
-      ?>
-    </div>
+    <?php the_content(); ?>
+  </div>
 
 <?php 
 // info box for EMBL CCO
 include( plugin_dir_path( __FILE__ ) . 'partials/event-info.php'); ?>
   
-</section>
-
-<section class="vf-content">
-    <?php 
-      if ($summary) {
-        the_content();
-      } ?>
-    
-    <?php if ( ! empty($additional_info)) { ?>
-        <h2>
-          <?php esc_html_e('Additional Information', 'vfwp'); ?>
-        </h2>
-        <p><?php echo ($additional_info); ?></p>
-    <?php } ?>    
 </section>
 
 <?php 
