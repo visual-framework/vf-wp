9b22bafc9b9fc8426b761257e41c78ca5e6524cd
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-10-30 12:37:35 +0100

    updates vf-navigation

--- wp-content/themes/vf-wp-groups/vf-wp-groups-header/vf-navigation.php
+++ wp-content/themes/vf-wp-groups/vf-wp-groups-header/vf-navigation.php
@@ -3,8 +3,8 @@
 if ( ! defined( 'ABSPATH' ) ) exit;
 
 ?>
-<nav class="vf-navigation vf-navigation--main">
-  <ul class="vf-navigation__list | vf-list--inline">
+<nav class="vf-navigation vf-navigation--main | vf-cluster">
+  <ul class="vf-navigation__list | vf-list--inline | vf-cluster__inner">
 <?php
 
 if (has_nav_menu('primary')) {
