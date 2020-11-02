fb34dccc1670051c3b4678b8f2828a6ed9eccd61
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2020-11-02 13:12:55 +0100

    moves vf-navigation to the top in the groups header

--- wp-content/themes/vf-wp-groups/vf-wp-groups-header/template.php
+++ wp-content/themes/vf-wp-groups/vf-wp-groups-header/template.php
@@ -41,15 +41,14 @@ if ($is_render) {
 
 ?>
 <header class="vf-header vf-header--inlay">
-  <?php
-  if ($masthead == 1) {
-    get_template_part('vf-wp-groups-header/vf-masthead');
-  }
-   ?>
   <?php if ($nav == 1) {
     get_template_part('vf-wp-groups-header/vf-navigation');
   }
-    ?>
+  ?>
+  <?php if ($masthead == 1) {
+    get_template_part('vf-wp-groups-header/vf-masthead');
+  }
+   ?>
 </header>
 <?php if ($is_hero) { ?>
 <?php get_template_part('vf-wp-groups-header/vf-hero'); ?>
