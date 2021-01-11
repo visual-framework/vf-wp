4aa97c453cca87eaaa18ee32c18c82eba3cba5d7
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-01-11 17:24:14 +0100

    updates functions

--- wp-content/themes/vf-wp-intranet/functions.php
+++ wp-content/themes/vf-wp-intranet/functions.php
@@ -2,6 +2,29 @@
 
 require_once('functions/vf-wp-intranet-breadcrumbs.php');
 
+// CHILD THEME CSS FILE
+
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+
+	$parent_style = 'parent-style';
+
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+	get_stylesheet_directory_uri() . '/style.css',
+	array( $parent_style ),
+	wp_get_theme()->get('Version')
+);
+}
+
+//ADDING CLASS TO A LINK IN CATEGORY
+//
+function add_class_to_category( $thelist, $separator, $parents){
+  $class_to_add = 'vf-link';
+  return str_replace('<a href="',  '<a class="'. $class_to_add. '" href="', $thelist);
+}
+
+add_filter('the_category', __NAMESPACE__ . '\\add_class_to_category',10,3);
 
 // adds support for feature images
 
