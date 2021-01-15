cdc3f3eda2b0054613637058b37e18876f039735
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-01-15 11:55:48 +0100

    adds vf class to links in categories list

--- wp-content/themes/vf-wp/functions.php
+++ wp-content/themes/vf-wp/functions.php
@@ -93,5 +93,14 @@ function vf_wp_theme__acf_settings_load_json($paths) {
   return $paths;
 }
 
+//Adds vf class to links in category list
+//
+function add_class_to_category( $thelist, $separator, $parents){
+  $class_to_add = 'vf-link';
+  return str_replace('<a href="',  '<a class="'. $class_to_add. '" href="', $thelist);
+}
+
+add_filter('the_category', __NAMESPACE__ . '\\add_class_to_category',10,3);
+
 
 ?>
