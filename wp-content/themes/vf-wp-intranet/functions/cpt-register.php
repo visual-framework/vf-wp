00755074e5f54bff35777a714da57c0a1762740a
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-01-25 14:47:45 +0100

    reisters documents cpt

--- wp-content/themes/vf-wp-intranet/functions/cpt-register.php
+++ wp-content/themes/vf-wp-intranet/functions/cpt-register.php
@@ -8,6 +8,86 @@
 
 add_action(
   'init',
+  'document_init_register'
+);
+
+  function document_init_register() {
+
+    register_post_type('documents', array(
+      'labels'              => document_get_labels(),
+      'description'         => __('Documents', 'vfwp'),
+      'public'              => true,
+      'hierarchical'        => true,
+      'exclude_from_search' => false,
+      'publicly_queryable'  => true,
+      'show_ui'             => true,
+      'show_in_menu'        => true,
+      'show_in_nav_menus'   => true,
+      'show_in_admin_bar'   => true,
+      'show_in_rest'        => true,
+      'rest_base'           => "document",
+      'menu_icon'           => 'dashicons-text-page',
+      'capability_type'     => 'page',
+      'supports'            => array('title', 'editor', 'page-attributes', 'excerpt', 'thumbnail'),
+      'has_archive'         => true,
+      'rewrite'             => array(
+        'slug' => 'document'
+      ),
+      'query_var'           => true,
+      'can_export'          => true,
+      'delete_with_user'    => false,
+      'taxonomies'          => array(
+        'embl-location'
+  
+      ),
+    ));
+
+  }
+  /**
+   * Reference: `get_post_type_labels`
+   * https://core.trac.wordpress.org/browser/tags/5.4/src/wp-includes/post.php
+   */
+  function document_get_labels() {
+    return array(
+      'name'                     => _x( 'Documents', 'Document type general name', 'vfwp' ),
+      'singular_name'            => _x( 'Document', 'Document type singular name', 'vfwp' ),
+      'add_new'                  => _x( 'Add New', 'Document', 'vfwp' ),
+      'add_new_item'             => __( 'Add New Document', 'vfwp' ),
+      'edit_item'                => __( 'Edit Document', 'vfwp' ),
+      'new_item'                 => __( 'New Document', 'vfwp' ),
+      'view_item'                => __( 'View Document', 'vfwp' ),
+      'view_items'               => __( 'View Documents', 'vfwp' ),
+      'search_items'             => __( 'Search Documents', 'vfwp' ),
+      'not_found'                => __( 'No Documents found.', 'vfwp' ),
+      'not_found_in_trash'       => __( 'No Documents found in Trash.', 'vfwp' ),
+      'parent_item_colon'        => __( 'Parent Page:', 'vfwp' ),
+      'all_items'                => __( 'All Documents', 'vfwp' ),
+      'archives'                 => __( 'Document Archives', 'vfwp' ),
+      'attributes'               => __( 'Document Attributes', 'vfwp' ),
+      'insert_into_item'         => __( 'Insert into Document', 'vfwp' ),
+      'uploaded_to_this_item'    => __( 'Uploaded to this Document', 'vfwp' ),
+      'featured_image'           => _x( 'Featured image', 'Document', 'vfwp' ),
+      'set_featured_image'       => _x( 'Set featured image', 'Document', 'vfwp' ),
+      'remove_featured_image'    => _x( 'Remove featured image', 'Document', 'vfwp' ),
+      'use_featured_image'       => _x( 'Use as featured image', 'Document', 'vfwp' ),
+      'filter_items_list'        => __( 'Filter Documents list', 'vfwp' ),
+      'items_list_navigation'    => __( 'Documents list navigation', 'vfwp' ),
+      'items_list'               => __( 'Documents list', 'vfwp' ),
+      'item_published'           => __( 'Document published.', 'vfwp' ),
+      'item_published_privately' => __( 'Document published privately.', 'vfwp' ),
+      'item_reverted_to_draft'   => __( 'Document reverted to draft.', 'vfwp' ),
+      'item_scheduled'           => __( 'Document scheduled.', 'vfwp' ),
+      'item_updated'             => __( 'Document updated.', 'vfwp' ),
+    );
+  }
+
+  /**
+   * Action: `init`
+   * Register the custom post type
+   */
+
+add_action(
+  'init',
   'insites_init_register'
 );
 
