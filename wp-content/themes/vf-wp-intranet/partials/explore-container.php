fbfdd7e14221e512c0b2535540cf2796a764b9cb
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-01-27 12:57:01 +0100

    links update

--- wp-content/themes/vf-wp-intranet/partials/explore-container.php
+++ wp-content/themes/vf-wp-intranet/partials/explore-container.php
@@ -1,7 +1,7 @@
 <section class="vf-summary-container | embl-grid embl-grid--has-centered-content | vf-u-background-color--grey--lightest vf-u-fullbleed vf-u-background-color-ui--off-white vf-u-padding__top--500 vf-u-padding__bottom--500  vf-u-margin__bottom--800">
 
 <div class="vf-section-header">
-  <a href="/search" class="vf-section-header__heading vf-section-header__heading--is-link">Search <svg class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path></svg></a>
+  <a href="/internal-information/search" class="vf-section-header__heading vf-section-header__heading--is-link">Search <svg class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path></svg></a>
 </div>
 
 <div class="vf-section-content">
