7f48d059c744819d20a41890d2b78fd558dc2dbb
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-03-04 08:40:05 +0100

    updates insight lectures templates

--- wp-content/themes/vf-wp-ells/archive-insight-lecture.php
+++ wp-content/themes/vf-wp-ells/archive-insight-lecture.php
@@ -7,28 +7,21 @@ $summary = get_field('il_summary');
 
 ?>
 
-<section class="vf-hero vf-hero--primary vf-hero--1200 | vf-u-fullbleed | vf-u-margin__bottom--0" style="--vf-hero--bg-image-size: auto 28.5rem">
-
-  <div class="vf-hero__content | vf-stack vf-stack--400 ">
-
+<section class="vf-hero vf-u-fullbleed | vf-u-margin__bottom--0" style="--vf-hero--bg-image-size: auto 28.5rem">
+  <div class="vf-hero__content | vf-box | vf-stack vf-stack--400">
     <h2 class="vf-hero__heading">
     EMBL Insight Lectures    </h2>
-
     <p class="vf-hero__text">Morbi dictum purus sit amet purus blandit, quis facilisis mauris semper</p>
-
   </div>
-
 </section>
 
 <?php
-
 if (class_exists('VF_Navigation')) {
   VF_Plugin::render(VF_Navigation::get_plugin('vf_navigation'));
 }
-
 ?>
 
-<div class="vf-grid vf-grid__col-4 | vf-content | vf-u-margin__bottom--800">
+<div class="vf-grid vf-grid__col-4 | vf-content | vf-u-margin__bottom--600">
   <div class="vf-grid__col--span-3">
     <h3>The lectures are live streamed to school across the globe and made available online after the event.
     </h3>
@@ -36,7 +29,6 @@ if (class_exists('VF_Navigation')) {
     </p>
   </div>
 </div>
-
 
   <?php $upcomingLecture = new WP_Query (array('posts_per_page' => 1, 'post_type' => 'insight-lecture'  ));
       $ids = array();
@@ -49,7 +41,7 @@ if (class_exists('VF_Navigation')) {
 <section class="vf-content">
   <h3>Browse or filter all Insight Lectures</h3>
   <div class="vf-grid vf-grid__col-4 | vf-u-padding__top--400">
-    <div class="vf-content">
+    <div>
       <?php include(locate_template('partials/lecture-filter.php', false, false)); ?>
     </div>
     <div class="vf-grid__col--span-3">
@@ -61,7 +53,6 @@ if (class_exists('VF_Navigation')) {
             if (($wp_query->current_post + 1) < ($wp_query->post_count)) {
               echo '<hr class="vf-divider">';
            }
-
           }
         } else {
           echo '<p>', __('No posts found', 'vfwp'), '</p>';
