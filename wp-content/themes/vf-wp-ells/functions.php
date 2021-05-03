19fdb247733ede27af918fc0b485ed2417f8f21e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-05-03 12:55:55 +0200

    removes ells newsletter continer

--- wp-content/themes/vf-wp-ells/functions.php
+++ wp-content/themes/vf-wp-ells/functions.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once('functions/custom-taxonomies.php');
-require_once('functions/ells-breadcrumbs.php');
 require_once('functions/learning-labs-post.php');
 require_once('functions/teachingbase-post.php');
 require_once('functions/insight-lecture-post.php');
@@ -16,33 +15,6 @@ add_theme_support( 'title-tag' );
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-class VF_ELLS_Newsletter extends VF_Plugin{
-
-  protected $file = __FILE__;
-
-  protected $config = array(
-    'post_name'  => 'vf_ells_newsletter',
-    'post_title' => 'ELLS_Newsletter',
-    'post_type'  => 'vf_container'
-  );
-
-  public function __construct(array $params = array()) {
-    parent::__construct('vf_ells_newsletter');
-    if (array_key_exists('init', $params)) {
-      parent::initialize();
-    }
-  }
-
-  public function template_callback($block, $content, $is_preview = false, $acf_id) {
-    ?><h3>to jest newslettter</h3><?php
-   }
-
-} // VF_ELLS_Newsletter
-
-$plugin = new VF_ELLS_Newsletter(array('init' => true));
-
-?>
-<?php
 // CHILD THEME CSS FILE
 
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
