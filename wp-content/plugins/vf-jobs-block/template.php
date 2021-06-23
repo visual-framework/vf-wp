714dadd9c4408bc7d2c8f32bf94867471646d6c0
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-06-23 12:02:35 +0200

    adds option to link to embl-ebi jobs

--- wp-content/plugins/vf-jobs-block/template.php
+++ wp-content/plugins/vf-jobs-block/template.php
@@ -5,6 +5,8 @@ $acf_id = isset($acf_id) ? $acf_id : false;
 $heading = get_field('vf_jobs_heading', $acf_id);
 $heading = trim($heading);
 
+$site = get_field('vf_jobs_site', $acf_id);
+
 $limit = get_field('vf_jobs_limit', $acf_id);
 $limit = intval($limit);
 $limit = $limit < 1 || $limit > 50 ? 50 : $limit;
@@ -100,8 +102,16 @@ $content = preg_replace(
     <?php if ( ! empty($heading)) {
     ?>
   <div class="vf-section-header">
-    <a class="vf-section-header__heading vf-section-header__heading--is-link" href="https://embl.org/jobs"
-      id="section-link"> <?php echo esc_html($heading); ?> <svg aria-hidden="true"
+    <a class="vf-section-header__heading vf-section-header__heading--is-link" 
+    <?php 
+    if ($site == 'embl-ebi') {
+      echo 'href="https://www.ebi.ac.uk/careers/jobs"';
+    } 
+    else {
+      echo 'href="https://embl.org/jobs"';
+    }
+    ?>
+        id="section-link"> <?php echo esc_html($heading); ?> <svg aria-hidden="true"
         class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24"
         xmlns="http://www.w3.org/2000/svg">
         <path
@@ -119,9 +129,6 @@ $content = preg_replace(
     echo '<!-- As no content was found for this query, the jobs header has also been hidden. -->';
   }
   echo $content . PHP_EOL;
-  if ($was_no_result_found < 1) {
-    echo '<p><a class="vf-link" href="//www.embl.org/jobs">View all EMBL jobs</a></p>' . PHP_EOL;
-  }
 ?>
 </div>
 </div>
