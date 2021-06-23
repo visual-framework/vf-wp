b91c1102fdba77f6ae99aafbe2346c31bb24913f
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-06-23 08:42:25 +0200

    adds survey banner

--- wp-content/themes/vf-wp-ells/front-page.php
+++ wp-content/themes/vf-wp-ells/front-page.php
@@ -5,62 +5,16 @@ get_header();
 global $vf_theme;
 
 ?>
+<div class="vf-banner vf-banner--alert vf-banner--info">
+  <div class="vf-banner__content">
+    <p class="vf-banner__text">We have a new website! <a class="vf-banner__link"
+        href="https://www.surveymonkey.de/r/new_ELLS_web">Give us feedback </a> and get exclusive preview access to a
+      resource collection on ocean diversity.</p>
+  </div>
+</div>
 
 <!-- SEARCH -->
-<?php /*
-<section
-  class="vf-grid vf-grid__col-4 | vf-u-fullbleed vf-u-padding__top--500 vf-u-padding__bottom--500 vf-u-margin__bottom--500 | vf-content">
-  <div style="text-align: center">
-    <img src="https://wwwdev.embl.org/ells/wp-content/uploads/2021/04/20200527_ELLS_VisualTag_RGB_COLOR.png"
-      style="max-width: 50%;"> </div>
 
-  <div class="vf-grid__col--span-2">
-
-    <h3 class="vf-text vf-text-heading--3 vf-u-margin__top--0">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sapien sem, tincidunt sed felis non, faucibus
-      aliquam ipsum. Aliquam convallis vitae nisl ac hendrerit.
-    </h3>
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec <a>sapien</a> sem, tincidunt sed felis non,
-      <a>convallis</a> faucibus aliquam ipsum. Aliquam <a>consectetur adipiscing</a> convallis vitae nisl ac hendrerit.
-    </p>
-  </div>
-  <div>
-    <form role="search" method="get" action="<?php echo esc_url(home_url('/')); ?>"
-      class="vf-form vf-form--search vf-form--search--mini | vf-sidebar vf-sidebar--end | vf-u-margin__bottom--600">
-      <div class="vf-sidebar__inner">
-
-        <div class="vf-form__item">
-
-          <label class="vf-form__label vf-u-sr-only | vf-search__label" for="searchitem">Search</label>
-          <input type="search" placeholder="Search ELLS" id="searchitem" class="vf-form__input"
-            value="<?php echo esc_attr(get_search_query()); ?>" name="s">
-        </div>
-
-        <button type="submit" class="vf-search__button | vf-button vf-button--primary"
-          value="<?php esc_attr_e('Search', 'vfwp'); ?>">
-          <span class="vf-button__text | vf-u-sr-only">Search</span>
-
-          <svg class="vf-icon vf-icon--search-btn | vf-button__icon" aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg" version="1.1" xmlns:xlink="http://www.w3.org/1999/xlink"
-            xmlns:svgjs="http://svgjs.com/svgjs" viewBox="0 0 140 140" width="140" height="140">
-            <g transform="matrix(5.833333333333333,0,0,5.833333333333333,0,0)">
-              <path
-                d="M23.414,20.591l-4.645-4.645a10.256,10.256,0,1,0-2.828,2.829l4.645,4.644a2.025,2.025,0,0,0,2.828,0A2,2,0,0,0,23.414,20.591ZM10.25,3.005A7.25,7.25,0,1,1,3,10.255,7.258,7.258,0,0,1,10.25,3.005Z"
-                fill="#FFFFFF" stroke="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="0"></path>
-            </g>
-          </svg>
-
-        </button>
-      </div>
-
-    </form>
-    <p><a href="#">Subscribe to our newsletter</a></p>
-
-  </div>
-</section>
-
-*/ ?>
 <section class="vf-summary-container | vf-grid vf-grid__col-4 | vf-u-fullbleed | vf-u-background-color-ui--off-white | vf-u-padding__top--500 vf-u-padding__bottom--500 vf-u-margin__bottom--500">
 <div>
 </div>
