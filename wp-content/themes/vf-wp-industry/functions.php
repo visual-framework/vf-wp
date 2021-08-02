bb50415d3eef2a5795129c3f5f71cd26b1c19db4
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-08-02 08:54:23 +0200

    event year parameter

--- wp-content/themes/vf-wp-industry/functions.php
+++ wp-content/themes/vf-wp-industry/functions.php
@@ -151,4 +151,71 @@ function event_custom_acf_field_link( $permalink, $post, $leavename ) {
 add_filter( 'post_type_link', 'event_custom_acf_field_link', 10, 3 );
 
 
+
+
+
+//
+
+
+
+// action
+// array of filters (field key => field name)
+$GLOBALS['my_query_filters'] = array( 
+	'field_61039f7766163'	=> 'event_year', 
+);
+
+
+// action
+add_action('pre_get_posts', 'my_pre_get_posts', 10, 1);
+
+function my_pre_get_posts( $query ) {
+	
+	// bail early if is in admin
+	if( is_admin() ) return;
+	
+	
+	// bail early if not main query
+	// - allows custom code / plugins to continue working
+	if( !$query->is_main_query() ) return;
+	
+	// get meta query
+	$meta_query = $query->get('meta_query');
+
+	// loop over filters
+	foreach( $GLOBALS['my_query_filters'] as $key => $name ) {
+		// continue if not found in url
+		if( empty($_GET[ $name ]) ) {
+			
+			continue;
+			
+		}
+		
+		
+		// get the value for this filter
+		// eg: http://www.website.com/events?city=melbourne,sydney
+		$value = explode(',', $_GET[ $name ]);
+
+
+
+
+        $meta_query =array();
+		// append meta query
+    	$meta_query[] = array(
+            'key'		=> $name,
+            'value'		=> $value,
+            'compare'	=> 'IN',
+        );
+        
+	} 
+	
+	
+	// update meta query
+	$query->set('meta_query', $meta_query);
+
+} 
+
+
+
+
+
 ?>
