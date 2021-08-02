5399a80e863d042bc4bcb897d0b802e627d91010
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-08-02 08:50:52 +0200

    front page template

---
+++ wp-content/themes/vf-wp-industry/index.php
@@ -0,0 +1,66 @@
+<?php
+
+// Use blog index template if no page is set for homepage
+if (get_option('show_on_front') !== 'page') {
+  get_template_part('index');
+  exit;
+} ?>
+
+<?php
+
+get_header();
+
+global $post;
+setup_postdata($post);
+
+global $vf_theme;
+$title = get_the_title();
+?>
+
+<?php
+
+
+
+$open_wrap = function($html, $block_name) {
+  $html = '
+<div class="embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+' . $html;
+return $html;
+};
+
+$close_wrap = function($html, $block_name) {
+  $html .= '
+  </div>
+  <div></div>
+</div>
+<!--/embl-grid-->';
+return $html;
+};
+
+add_filter(
+'vf/__experimental__/theme/content/open_block_wrap',
+$open_wrap,
+10, 2
+);
+
+add_filter(
+'vf/__experimental__/theme/content/close_block_wrap',
+$close_wrap,
+10, 2
+);
+
+?>
+
+<?php
+
+$vf_theme->the_content();
+
+?>
+
+<?php
+
+get_footer();
+
+?>
