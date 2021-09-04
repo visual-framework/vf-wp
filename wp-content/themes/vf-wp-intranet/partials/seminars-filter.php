f554c864baeb1b972f57e929fd529061b7cc6666
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-09-04 09:54:51 +0200

    seminars - initial work

---
+++ wp-content/themes/vf-wp-intranet/partials/seminars-filter.php
@@ -0,0 +1,19 @@
+<form class="vf-stack vf-stack-400 | vf-u-margin__bottom--400">
+  <fieldset class="vf-form__fieldset vf-stack vf-stack--400">
+    <label class="vf-form__label">Location</label>
+    <div class="vf-form__item vf-form__item--checkbox">
+      <input id="location-1" type="checkbox" data-jplist-control="checkbox-text-filter"
+        data-path=".location" data-group="data-group-1" data-name="name" data-or="name"
+        value="EMBL Heidelberg"
+        data-id="name-heidelberg" class="vf-form__checkbox">
+      <label for="location-1" class="vf-form__label">EMBL Heidelberg</label>
+    </div>
+    <div class="vf-form__item vf-form__item--checkbox">
+      <input id="location-2" type="checkbox" data-jplist-control="checkbox-text-filter"
+        data-path=".location" data-group="data-group-1" data-name="name" data-or="name"
+        value="virtual"
+        data-id="name-virtual" class="vf-form__checkbox">
+      <label for="location-2" class="vf-form__label">Virtual</label>
+    </div>
+  </fieldset>
+</form>
---
+++ wp-content/themes/vf-wp-intranet/template-seminars.php
@@ -0,0 +1,132 @@
+<?php
+
+/**
+* Template Name: Seminars
+*/
+
+get_header();
+
+?>
+
+
+<section class="vf-intro | vf-u-margin__bottom--0">
+  <div>
+    <!-- empty -->
+  </div>
+  <div class="vf-stack">
+    <h1 class="vf-intro__heading">
+      <?php wp_title(''); ?>
+    </h1>
+  </div>
+</section>
+
+<div
+  class="embl-grid embl-grid--has-centered-content | vf-u-padding__top--500 vf-u-padding__bottom--500 | vf-u-margin__bottom--800">
+  <div></div>
+  <form action="#eventsFilter" onsubmit="return false;"
+    class="vf-form vf-form--search vf-form--search--responsive | vf-sidebar vf-sidebar--end">
+    <div class="vf-sidebar__inner">
+      <div class="vf-form__item">
+        <label class="vf-form__label vf-u-sr-only | vf-search__label" for="textbox-filter">Search</label>
+        <input id="textbox-filter" data-jplist-control="textbox-filter" data-group="data-group-1"
+          data-name="my-filter-1" data-path=".vf-summary__title" data-id="search" type="text" value=""
+          placeholder="Filter by document title" data-clear-btn-id="name-clear-btn"
+          class="vf-form__input | vf-search__input" />
+      </div>
+      <button href="#eventsFilter" class="vf-search__button | vf-button vf-button--primary">
+        <span class="vf-button__text">Filter</span>
+      </button>
+    </div>
+  </form>
+</div>
+
+<section class="embl-grid embl-grid--has-centered-content">
+  <div>
+    <?php include(locate_template('partials/seminars-filter.php', false, false)); ?>
+  </div>
+  <div>
+    <div class="vf-content" >
+    <div class="embl-content-hub-loader | vf-grid vf-grid__col-1" data-jplist-group="data-group-1">
+        <?php
+$request = wp_remote_get( 'https://www.embl.org/api/v1/events?_format=json&source=contenthub&filter-field-value[field_event_type]=Seminar&sort-field-value[field_event_start_date_time.value]=ASC&filter-field-date-after[field_event_start_date_time]=today
+' );
+
+if( is_wp_error( $request ) ) {
+	return false; // Bail early
+}
+
+$body = wp_remote_retrieve_body( $request );
+
+$data = json_decode( $body );
+
+if( ! empty( $data ) ) {
+	
+	echo '<ul>';
+	foreach( $data as $product ) {
+		echo '<article data-jplist-item>';
+			echo '<p class="vf-summary__title">' . $product->title . '</p>';
+			echo '<p class="vf-summary__location | location">' . $product->field_event_location . '</p>';
+		echo '</article>';
+	}
+	echo '</ul>';
+}
+
+        ?>
+  </div>
+
+      <!-- no results control -->
+      <article class="vf-summary vf-summary--event" data-jplist-control="no-results" data-group="data-group-1"
+        data-name="no-results">
+        <p class="vf-summary__text">
+          No matching documents found
+        </p>
+      </article>
+    </div>
+
+    <style>
+      .jplist-selected {
+        background-color: #707372;
+      }
+
+      .jplist-selected a {
+        color: #fff;
+      }
+
+    </style>
+
+    <nav class="vf-pagination" aria-label="Pagination" data-jplist-control="pagination" data-group="data-group-1"
+      data-items-per-page="10" data-current-page="0" data-name="pagination1">
+      <ul class="vf-pagination__list">
+        <li class="vf-pagination__item vf-pagination__item--previous-page" data-type="prev">
+          <a class="vf-pagination__link">
+            Previous<span class="vf-u-sr-only"> page</span>
+          </a>
+        </li>
+        <div data-type="pages" style="display: flex;">
+          <li class="vf-pagination__item" data-type="page">
+            <a href="#" class="vf-pagination__link">
+              {pageNumber}<span class="vf-u-sr-only">page</span>
+            </a>
+          </li>
+        </div>
+        <li class="vf-pagination__item vf-pagination__item--next-page" data-type="next">
+          <a href="#" class="vf-pagination__link">
+            Next<span class="vf-u-sr-only"> page</span>
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+  </div>
+  <div></div>
+</section>
+
+<script type="text/javascript">
+  jplist.init();
+</script>
+
+<?php
+
+get_footer();
+
+?>
