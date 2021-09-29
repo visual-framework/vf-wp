4ab90f5aad374d829a990e946ffc840a43347505
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-09-29 10:42:49 +0200

    updates deprecated functions in WP 5.8

--- wp-content/themes/vf-wp/functions.php
+++ wp-content/themes/vf-wp/functions.php
@@ -137,9 +137,9 @@ add_action( 'admin_head', 'remove_margin_wp_toolbar' );
 add_action( 'wp_head', 'remove_margin_wp_toolbar' );
 
 // allow only certain WP Gutenberg blocks
-add_filter( 'allowed_block_types', 'allowed_block_types' );
+add_filter( 'allowed_block_types_all', 'allowed_block_types' );
  
-function allowed_block_types( $allowed_blocks ) {
+function allowed_block_types($allowed_blocks) {
  
 	return array(
 		'core/image',
