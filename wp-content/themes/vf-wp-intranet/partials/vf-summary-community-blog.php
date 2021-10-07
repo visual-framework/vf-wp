a5e029cf2eb8d9b47c8e4c5a0efa5cd12fc589ad
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-10-07 12:56:53 +0200

    updates community blog homepage container

---
+++ wp-content/themes/vf-wp-intranet/partials/vf-summary-community-blog.php
@@ -0,0 +1,40 @@
+<?php
+$topic_terms = get_field('cb_topic');
+$locations = get_field('cb_embl_location');
+
+?>
+<article class="vf-summary vf-summary--news" style="display: block; display: unset;">
+  <span class="vf-summary__date"><time class="vf-summary__date vf-u-text-color--grey" style="margin-left: 0;"
+      title="<?php the_time('c'); ?>"
+      datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?></time></span>
+  <h3 class="vf-summary__title">
+    <a href="<?php the_permalink(); ?>" class="vf-summary__link"><?php echo esc_html(get_the_title()); ?></a>
+  </h3>
+  <p class="vf-summary__text | vf-u-margin__bottom--200">
+    <?php if (($topic_terms)) { ?>
+    <span class="vf-summary__category">
+      <span>
+        <?php 
+        if( $topic_terms ) {
+          $topics_list = array(); 
+          foreach( $topic_terms as $term ) {
+            $topics_list[] = '<a class="vf-link" style="color: #707372;" href="' . esc_url(get_term_link( $term )) . '" class="vf-link">' . strtoupper(esc_html( $term->name )) . '</a>'; }
+            echo implode(', ', $topics_list); }?>
+      </span>
+      <?php } 
+    if (($locations) && ($topic_terms))
+    { ?>
+      <span style="color: #000">&nbsp;|&nbsp;</span>
+      <?php
+    }
+    if (($locations)) { ?>
+      <span class="vf-u-text-color--grey | location" style="text-transform: none;">
+        <?php $location_list = [];
+        foreach( $locations as $location ) { 
+          $location_list[] = $location->name; }
+          echo implode(', ', $location_list); ?>
+      </span>
+    </span>
+  </p>
+  <?php } ?>
+</article>
