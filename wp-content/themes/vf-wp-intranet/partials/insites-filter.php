68fe7bfa429e3b52c0a661bd647e9cb01f0e958e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-10-15 11:54:13 +0200

    updates insites filter to have 'All' checked by default

--- wp-content/themes/vf-wp-intranet/partials/insites-filter.php
+++ wp-content/themes/vf-wp-intranet/partials/insites-filter.php
@@ -1,5 +1,5 @@
 <?php
-$counter = 1;
+$counter = 2;
 
 // Get all "location" terms
 $embl_location_terms = get_terms(
@@ -21,8 +21,16 @@ $topic_terms = get_terms(
 <form class="vf-stack vf-stack-400 | vf-u-margin__bottom--400">
   <fieldset class="vf-form__fieldset vf-stack vf-stack--400">
   <legend class="vf-form__legend">EMBL site</legend>
+  <div class="vf-form__item vf-form__item--checkbox">
+      <input id="location-1" type="checkbox" data-jplist-control="checkbox-text-filter"
+        data-path=".location" data-group="data-group-1" data-name="name1" data-or="name1"
+        value="all"
+        data-id="name1-all" class="vf-form__checkbox" checked>
+      <label for="location-1" class="vf-form__label">All EMBL Sites</label>
+    </div>
+
     <?php
-    foreach($embl_location_terms as $term) {
+    foreach(array_slice($embl_location_terms,1) as $term) {
       ?>
     <div class="vf-form__item vf-form__item--checkbox">
       <input id="location-<?php echo $counter; ?>" type="checkbox" data-jplist-control="checkbox-text-filter"
