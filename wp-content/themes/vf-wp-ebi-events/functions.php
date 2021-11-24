e1a559a51108766ff987aff790f5feffec2492dd
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-11-24 15:49:53 +0100

    misc updates

--- wp-content/themes/vf-wp-ebi-events/functions.php
+++ wp-content/themes/vf-wp-ebi-events/functions.php
@@ -16,6 +16,20 @@ function vf_wp_documents__acf_settings_load_json($paths) {
     return $paths;
   }
 
+/* display acf in the menu */
+
+add_filter('acf/settings/remove_wp_meta_box', '__return_false');
+
+add_filter('acf/settings/show_admin', '__return_true');
+function my_acf_save_post( $post_id ) {
+
+    $user = get_field( 'author', $post_id );
+	if( $user ) {
+		wp_update_post( array( 'ID'=>$post_id, 'post_author'=>$user['ID']) );
+	}
+}
+add_action('acf/save_post', 'my_acf_save_post', 20);
+
 /* Theme CSS file */
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
