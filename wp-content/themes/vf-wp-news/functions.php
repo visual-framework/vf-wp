15e8238b2f27bd64da88901751a9fe3e30b964ed
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2021-11-25 19:53:43 +0100

    adds featured image to  news REST API

--- wp-content/themes/vf-wp-news/functions.php
+++ wp-content/themes/vf-wp-news/functions.php
@@ -321,4 +321,23 @@ function get_display_api_field($post)
     return get_field('field_target_display', $post['id']);
 }
 
+// Add featured image to REST API
+add_action('rest_api_init', 'register_rest_images' );
+function register_rest_images(){
+    register_rest_field( array('post'),
+        'fimg_url',
+        array(
+            'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+}
+function get_rest_featured_image( $object, $field_name, $request ) {
+    if( $object['featured_media'] ){
+        $img = wp_get_attachment_image_src( $object['featured_media'], 'app-thumb' );
+        return $img[0];
+    }
+    return false;
+}
 ?>
