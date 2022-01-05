78de77f03ab69d1d32ad6d18e3d311c303613b4e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-01-05 10:23:41 +0100

    adds a link to internal newsletter

--- wp-content/themes/vf-wp-intranet/archive-insites.php
+++ wp-content/themes/vf-wp-intranet/archive-insites.php
@@ -19,11 +19,10 @@ $title = $vf_theme->get_title();
   </div>
 </section>
 
-<section
-  class="vf-news-container vf-news-container--featured | vf-u-margin__bottom--100 | vf-u-padding__top--400">
+<section class="vf-news-container vf-news-container--featured | vf-u-margin__bottom--100 | vf-u-padding__top--400">
   <h2 class="vf-section-header__heading vf-u-margin__bottom--400">Featured</h2>
   <div class="vf-news-container__content vf-grid vf-grid__col-4">
-  <?php
+    <?php
 			$featured = new WP_Query (array(
         'posts_per_page' => 4, 
         'post_type' => 'insites', 
@@ -34,7 +33,7 @@ $title = $vf_theme->get_title();
       $ids[] = get_the_ID(); ?>
     <?php include(locate_template('partials/vf-summary-insites-featured.php', false, false)); ?>
     <?php endwhile;?>
-    <?php wp_reset_postdata(); ?>  </div>
+    <?php wp_reset_postdata(); ?> </div>
 </section>
 
 <hr class="vf-divider">
@@ -66,7 +65,7 @@ $title = $vf_theme->get_title();
   </div>
   <div>
     <div data-jplist-group="data-group-1">
-    <?php
+      <?php
         if ( have_posts() ) {
           while ( have_posts() ) {
             the_post();
@@ -117,21 +116,40 @@ $title = $vf_theme->get_title();
     </nav>
 
   </div>
-  <div>
-  <article class="vf-card vf-card--brand vf-card--bordered">
-<div class="vf-card__content | vf-stack vf-stack--400">
-  <h3 class="vf-card__heading"><a class="vf-card__link" href="https://www.embl.org/news/">EMBL News<svg aria-hidden="true" class="vf-card__heading__icon | vf-icon vf-icon-arrow--inline-end" width="1em" height="1em" xmlns="http://www.w3.org/2000/svg">
-        <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="currentColor" fill-rule="nonzero"></path>
-      </svg>
-    </a></h3>
-    <p class="vf-card__text">Latest updates from EMBL's six sites</p>
-</div>
-</article>
+  <div class="vf-stack vf-stack--400">
+    <article class="vf-card vf-card--brand vf-card--bordered">
+      <div class="vf-card__content | vf-stack vf-stack--400">
+        <h3 class="vf-card__heading"><a class="vf-card__link" href="https://www.embl.org/news/">EMBL News<svg
+              aria-hidden="true" class="vf-card__heading__icon | vf-icon vf-icon-arrow--inline-end" width="1em"
+              height="1em" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z"
+                fill="currentColor" fill-rule="nonzero"></path>
+            </svg>
+          </a></h3>
+        <p class="vf-card__text">Latest updates from EMBL's six sites</p>
+      </div>
+    </article>
+    <article class="vf-card vf-card--brand vf-card--bordered">
+      <div class="vf-card__content | vf-stack vf-stack--400">
+        <h3 class="vf-card__heading"><a class="vf-card__link"
+            href="https://www.embl.org/internal-information/topic/internal-newsletter/">Internal newsletter<svg
+              aria-hidden="true" class="vf-card__heading__icon | vf-icon vf-icon-arrow--inline-end" width="1em"
+              height="1em" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z"
+                fill="currentColor" fill-rule="nonzero"></path>
+            </svg>
+          </a></h3>
+        <p class="vf-card__text">Previous editions of the fortnightly publications</p>
+      </div>
+    </article>
   </div>
 </section>
 
 <script type="text/javascript">
   jplist.init();
+
 </script>
 
 <?php
