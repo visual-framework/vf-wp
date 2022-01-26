d2d2d9a491f6ec34a3f29a222a2be09670505f3f
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-01-26 11:21:22 +0100

    adds counter and selects first link

--- wp-content/themes/vf-wp/blocks/vfwp-navigation-on-this-page/template.php
+++ wp-content/themes/vf-wp/blocks/vfwp-navigation-on-this-page/template.php
@@ -20,24 +20,32 @@ $admin_banner = function($message, $modifier = 'info') use ($is_preview) {
 <!--/vf-banner-->
 <?php
 };
-global $post;
+
+if (
+  ! have_rows('vf-wp-navigation-otp')
+) {
+  $admin_banner(__('Please add a link.', 'vfwp'));
+  return;
+}
 
 ?>
 <nav class="vf-navigation vf-navigation--on-this-page | vf-u-fullbleed | vf-cluster">
-  <?php if (have_rows('vf-wp-navigation-otp')) { ?>
-    <ul class="vf-navigation__list | vf-list | vf-cluster__inner" data-vf-js-navigation-on-this-page-container="false">
+  <?php 
+  $counter = 0;
+  if (have_rows('vf-wp-navigation-otp')) { ?>
+    <ul class="vf-navigation__list | vf-list | vf-cluster__inner" data-vf-js-navigation-on-this-page-container="true">
     <li class="vf-navigation__item">
-On this page
-</li>
-      <?php
-      while (have_rows('vf-wp-navigation-otp')) {
-        the_row();
-        $link = get_sub_field('vf-wp-navigation-otp-link');
-      ?>
-      <li class="vf-navigation__item">
-        <a class="vf-navigation__link" href="<?php echo esc_url($link['url']); ?>"><?php echo esc_html($link['title']); ?></a>
-      </li>
-        <?php } ?>
+    On this page
+    </li>
+   <?php
+   while (have_rows('vf-wp-navigation-otp')) {
+     the_row();
+     $link = get_sub_field('vf-wp-navigation-otp-link');
+   ?>
+   <li class="vf-navigation__item">
+     <a class="vf-navigation__link" href="<?php echo esc_url($link['url']); ?>" <?php if( $counter == 0 ) { echo 'aria-selected="true"';}?>><?php echo esc_html($link['title']); ?></a>
+   </li>
+     <?php $counter++; } ?>
     </ul>
   <?php } ?>
 </nav>
