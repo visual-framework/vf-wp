4d31cbb7f77831edb48df2ae3792e43d1e9139de
Sandeep Kadam <sandeep@ebi.ac.uk>
2022-02-15 16:57:53 +0000

    Issue-653 Removed news theme as it will be in news repo now

--- wp-content/themes/vf-wp-news/header.php
+++
@@ -1,8 +0,0 @@
-<?php get_template_part('partials/head'); ?>
-<?php vf_header(); ?>
-
-<?php 
-if (class_exists('VF_Breadcrumbs')) {
-  VF_Plugin::render(VF_Plugin::get_plugin('vf_breadcrumbs'));
-}
-?>
