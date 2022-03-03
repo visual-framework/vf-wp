90e7adee96322f9f3c47bb93973970a303803e6e
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-03-03 13:47:47 +0100

    adds section header descriptions

--- wp-content/themes/vf-wp-intranet/partials/community-blog-container.php
+++ wp-content/themes/vf-wp-intranet/partials/community-blog-container.php
@@ -7,7 +7,7 @@
           d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z"
           fill="" fill-rule="nonzero"></path>
       </svg></a>
-    <p class="vf-section-header__text">Internal announcements and important updates from the EMBL community</p>
+    <p class="vf-section-header__text">Internal announcements and important updates from departments and teams. You can filter by site and topic.</p>
   </div>
 
   <div class="vf-section-content | vf-u-margin__top--200">
--- wp-content/themes/vf-wp-intranet/partials/seminars-container.php
+++ wp-content/themes/vf-wp-intranet/partials/seminars-container.php
@@ -41,6 +41,8 @@ function sortByStartDate($param1, $param2) {
               d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z"
               fill="" fill-rule="nonzero"></path>
           </svg></a>
+          <p class="vf-section-header__text" style="color: #fff;">Find here all the seminars organised at the EMBL. Most have been moved to hybrid format to allow all sites to participate.
+          </p>
       </div>
       <div class="vf-section-content | vf-u-margin__top--600">
         <div class="vf-grid vf-grid__col-4 | vf-content">
