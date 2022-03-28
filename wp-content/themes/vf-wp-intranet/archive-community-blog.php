1543fb021fac25b81933aca6ec440dadbb7350ad
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-03-28 09:50:44 +0200

    increases the number of posts displayed

--- wp-content/themes/vf-wp-intranet/archive-community-blog.php
+++ wp-content/themes/vf-wp-intranet/archive-community-blog.php
@@ -74,7 +74,7 @@ $title = $vf_theme->get_title();
     </style>
 
     <nav class="vf-pagination" aria-label="Pagination" data-jplist-control="pagination" data-group="data-group-1"
-      data-items-per-page="10" data-current-page="0" data-name="pagination1">
+      data-items-per-page="50" data-current-page="0" data-name="pagination1">
       <ul class="vf-pagination__list">
         <li class="vf-pagination__item vf-pagination__item--previous-page" data-type="prev">
           <a class="vf-pagination__link">
