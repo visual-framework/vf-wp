55a51227cb054bd4bc39021ed63140d180044dea
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-10-13 12:19:52 +0200

    updates h2 to h1 in vf-wp-groups-header

--- wp-content/themes/vf-wp-groups/vf-wp-groups-header/vf-hero.php
+++ wp-content/themes/vf-wp-groups/vf-wp-groups-header/vf-hero.php
@@ -56,11 +56,11 @@ $spacing_class .= "{$spacing}";
       | <a href="<?php echo esc_url($add_heading_2['url']); ?>"><?php echo esc_html($add_heading_2['title']); ?></a>
     </p>
     <?php } }?>
-    <h2 class="vf-hero__heading">
+    <h1 class="vf-hero__heading">
       <a class="vf-hero__heading_link" href="<?php echo get_home_url(); ?>">
         <?php echo $vf_plugin->get_hero_heading(); ?>
       </a>
-    </h2>
+    </h1>
 
     <p class="vf-hero__subheading"><?php echo $vf_plugin->get_hero_text(); ?></p>
 
