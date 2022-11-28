35bbcdffdc8867f7bcee8d7922da21e678094423
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2022-11-28 12:56:42 +0100

    updates profiles urls 2

--- wp-content/themes/vf-wp-intranet/partials/newcomers-summary.php
+++ wp-content/themes/vf-wp-intranet/partials/newcomers-summary.php
@@ -1,5 +1,11 @@
-<?php echo '<p>
-        <span><a href="' . $person->personUrl . '">' . $person->displayName . '</a></span><span>, ' . $person->title . ', </span> 
+<?php
+        $peopleBaseUrl = "https://www.embl.org/internal-information/people/";
+        $peopleSlug = basename($person->personUrl);
+        $peopleEndUrl = $peopleBaseUrl . $peopleSlug;
+        
+
+echo '<p>
+        <span><a href="' . $peopleEndUrl . '">' . $person->displayName . '</a></span><span>, ' . $person->title . ', </span> 
         <span>' . $person->orgUnitName . '</span> 
       </p>
 '; ?>
