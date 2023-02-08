70117d9b122b47b174acaa24c40623ae6c1fb0c5
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2023-02-08 14:05:14 +0100

    adds additional template for publications

--- wp-content/themes/vf-wp-groups/template-publications-filter.php
+++ wp-content/themes/vf-wp-groups/template-publications-filter.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* Template Name: Publications
+* Template Name: Publications with filter
 */
 
 // The plugin is missing so use the default page template
@@ -19,11 +19,41 @@ global $vf_theme;
 
 ?>
 
-<div class="vf-grid vf-grid__col-3 | vf-u-grid-gap--800">
-  <div class="vf-grid__col--span-2">
-    <h1 class="vf-text vf-text-heading--1">
+<div class="embl-grid embl-grid--has-centered-content | vf-u-margin__bottom--800">
+  <div></div>
+  <div>
+  <h1 class="vf-text vf-text-heading--1">
       <?php the_title(); ?>
     </h1>
+    <div class="vf-form__item">
+          <input id="search" class="vf-form__input vf-form__input--filter" data-jplist-control="textbox-filter"
+            data-group="data-group-1" data-name="my-filter-1" data-path=".vf-summary__title" type="text" value=""
+            placeholder="Filter by title" data-clear-btn-id="name-clear-btn">
+   </div>
+  </div>
+</div>
+
+<div class="embl-grid embl-grid--has-centered-content">
+  <div>
+  <form class="vf-stack vf-stack-400 | vf-u-margin__bottom--400">
+  
+
+  <fieldset class="vf-form__fieldset vf-stack vf-stack--400">
+  <legend class="vf-form__legend">Select year</legend>
+    <div class="vf-form__item vf-stack">
+  <select class="vf-form__select" id="vf-form__select" data-jplist-control="select-filter" data-group="data-group-1">
+  <option value="0" data-path="default" data-name="default" data-group="data-group-1">All</option>
+          <option data-path=".vf-summary__date" value="2022" data-name="default" data-group="data-group-1">2022</option>
+          </select>
+    </div>
+  </fieldset>
+</form>
+  </div>
+  <div>
+
+
+   <div data-jplist-group="data-group-1">
+
     <?php
 
       VF_Plugin::render($vf_publications);
@@ -71,7 +101,15 @@ global $vf_theme;
     </form>
     */ ?>
   </div>
+  </div>
 </div>
+
+<script type="text/javascript">
+  jplist.init({deepLinking: true});
+
+</script>
+
+
 <?php
 
 get_footer();
