05ef38c4cedb1bcf3a5e062e2794467190cd7704
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2023-08-24 07:08:22 +0200

    removes Klaipeda stop from trec events list

--- wp-content/themes/vf-wp-groups/partials/trec-filter.php
+++ wp-content/themes/vf-wp-groups/partials/trec-filter.php
@@ -4,7 +4,6 @@ $cities = ['Aarhus',
 'Bergen',
 'Bilbao',
 'Galway',
-'Klaipeda',
 'Kotor',
 'Kristineberg',
 'Liepaja',
@@ -22,22 +21,19 @@ $cities = ['Aarhus',
 'Turku']; ?>
 
 <form class="vf-stack vf-stack-400 | vf-u-margin__bottom--400">
-      <fieldset class="vf-form__fieldset vf-stack vf-stack--400">
-        <legend class="vf-form__legend">Location</legend>
-        <div class="vf-form__item vf-stack">
-          <select class="vf-form__select" id="vf-form__select" data-jplist-control="select-filter"
-            data-group="data-group-1">
-            <option value="0" data-path="default" data-name="default" data-group="data-group-1">All</option>
-            <?php foreach ($cities as $city):
+  <fieldset class="vf-form__fieldset vf-stack vf-stack--400">
+    <legend class="vf-form__legend">Location</legend>
+    <div class="vf-form__item vf-stack">
+      <select class="vf-form__select" id="vf-form__select" data-jplist-control="select-filter"
+        data-group="data-group-1">
+        <option value="0" data-path="default" data-name="default" data-group="data-group-1">All</option>
+        <?php foreach ($cities as $city):
               $cityFormated = str_replace("/","-",$city); ?>
-               
-<option value="city-<?php echo $cityFormated; ?>" data-path=".city-<?php echo $cityFormated; ?>" data-name="default" data-group="data-group-1"><?php echo $city; ?>
-</option>
-
+        <option value="city-<?php echo $cityFormated; ?>" data-path=".city-<?php echo $cityFormated; ?>"
+          data-name="default" data-group="data-group-1"><?php echo $city; ?>
+        </option>
         <?php endforeach; ?>
-
-          </select>
-        </div>
-      </fieldset>
-    </form>
-
+      </select>
+    </div>
+  </fieldset>
+</form>
