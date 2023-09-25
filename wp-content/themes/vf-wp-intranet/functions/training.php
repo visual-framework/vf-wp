2230a125f840c71f37060918ff45fd26c7b211c8
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2023-09-25 09:50:26 +0200

    clean ups settings page

--- wp-content/themes/vf-wp-intranet/functions/training.php
+++ wp-content/themes/vf-wp-intranet/functions/training.php
@@ -43,66 +43,50 @@ function settings_training_admin_page() {
   echo '<div class="wrap">';
   echo '<h2>Settings</h2>';
   settings_errors();
-  // Check whether the button has been pressed AND also check the nonce
+
+  // Unpublish
   if (isset($_POST['unpublish_training']) && check_admin_referer('unpublish_training_clicked')) {
-    // the button has been pressed AND we've passed the security check
     expire_trainings_function();
   }
   echo '<h3>Unpublish past trainings manually</h2>';
-
-
   echo '<form action="edit.php?post_type=training&page=training-slug" method="post">';
-  // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
   wp_nonce_field('unpublish_training_clicked');
   echo '<input type="hidden" value="true" name="unpublish_training" />';
   submit_button('Unpublish');
   echo '</form>';
 
-
-
-  // Check whether the button has been pressed AND also check the nonce
+  // EMBL-EBI
   if (isset($_POST['sync_training']) && check_admin_referer('sync_training_clicked')) {
-    // the button has been pressed AND we've passed the security check
     vfwp_intranet_cron_process_training_data();
   }
   echo '<h3>Sync EMBL-EBI Data Science Trainings data</h3>';
   echo '<form action="edit.php?post_type=training&page=training-slug" method="post">';
-  // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
   wp_nonce_field('sync_training_clicked');
   echo '<input type="hidden" value="true" name="sync_training" />';
   submit_button('Sync');
   echo '</form>';
 
-
-
+// BioIT
   echo '<h3>Sync BioIT trainings</h3>';
-  // Check whether the button has been pressed AND also check the nonce
   if (isset($_POST['sync_bioit_training']) && check_admin_referer('sync_bioit_training_clicked')) {
-    // the button has been pressed AND we've passed the security check
     vfwp_intranet_cron_process_bioit_data();
     }
   echo '<form action="edit.php?post_type=training&page=training-slug" method="post">';
-  // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
   wp_nonce_field('sync_bioit_training_clicked');
   echo '<input type="hidden" value="true" name="sync_bioit_training" />';
   submit_button('Sync BioIT');
   echo '</form>';
 
-
-
+// Delete
   echo '<h3>Delete all training posts</h3>';
-  // Check whether the button has been pressed AND also check the nonce
   if (isset($_POST['delete_training']) && check_admin_referer('delete_training_clicked')) {
-    // the button has been pressed AND we've passed the security check
     delete_all_training_posts();
   }
   echo '<form action="edit.php?post_type=training&page=training-slug" method="post">';
-  // this is a WordPress security feature - see: https://codex.wordpress.org/WordPress_Nonces
   wp_nonce_field('delete_training_clicked');
   echo '<input type="hidden" value="true" name="delete_training" />';
   submit_button('Delete');
   echo '</form>';
-
   echo '</div>';
 
 }
@@ -128,5 +112,6 @@ function my_acf_fields_relationship_result( $text, $post, $field, $post_id ) {
     }
     return $text;
 }
+
 
 ?>
