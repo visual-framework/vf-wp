1c8e54f9ae6942bf47ccca9c3aeb5b9afb1eefbf
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2023-10-10 16:01:17 +0200

    gets select fields values

--- wp-content/themes/vf-wp-intranet/template-my-hr-contact.php
+++ wp-content/themes/vf-wp-intranet/template-my-hr-contact.php
@@ -7,8 +7,16 @@
 
 get_header();
 
-global $vf_theme;
-$today_date = date('Ymd');
+$hrTopics = get_field_object('field_6524fc78fb1ba');
+$hrTopicsChoices = $hrTopics['choices'];
+
+$hrSite = get_field_object('field_6524fdddfb1bb');
+$hrSiteChoices = $hrSite['choices'];
+
+$hrGroup = get_field_object('field_6524fef8fb1bc');
+$hrGroupChoices = $hrGroup['choices'];
+
+
 ?>
 
 <div class="title-container">
@@ -18,48 +26,55 @@ $today_date = date('Ymd');
 <?php the_content(); ?>
 
 
-<div class="vf-grid | vf-grid__col-3">
+<div class="vf-grid | vf-grid__col-3 | vf-u-margin__top--0">
   <div class="vf-grid__col--span-2">
-    <div id="hrFilters" class="vf-u-margin__bottom--800">
+    <div id="hrFilters" class="vf-u-margin__bottom--800 | vf-u-background-color-ui--off-white | vf-u-padding--400">
   <div class="vf-form__item vf-stack">
+<label class="vf-form__label" for="vf-form__select-topic">Topic</label>
+  <select class="vf-form__select | selectCustomWidth" id="vf-form__select-topic" data-jplist-control="select-filter" data-group="data-group-1">
+    <option value="0" data-path="default" >Select</option>
+    <?php
+    foreach($hrTopicsChoices as $value => $label) {
+      ?>
+      <option  
+        data-path=".<?php echo esc_attr($value); ?>" 
+        value="<?php echo esc_attr($value); ?>"><?php echo esc_html($label); ?> </option>
+      <?php } ?>
+    </select>
 
-<label class="vf-form__label" for="vf-form__select">Topic</label>
-
-<select class="vf-form__select | selectCustomWidth" id="vf-form__select">
-  <option value="cat">Cat</option>
-  <option value="hamster">Hamster</option>
-  <option value="parrot">Parrot</option>
-  <option value="dog" selected>Dog</option>
-  <option value="spider">Spider</option>
-  <option value="goldfish">Goldfish</option>
-</select>
 
 </div>
 <div class="vf-form__item vf-stack">
 
-  <label class="vf-form__label" for="vf-form__select">EMBL site</label>
+  <label class="vf-form__label" for="vf-form__select-site">EMBL site</label>
 
-  <select class="vf-form__select | selectCustomWidth" id="vf-form__select">
-    <option value="cat">Cat</option>
-    <option value="hamster">Hamster</option>
-    <option value="parrot">Parrot</option>
-    <option value="dog" selected>Dog</option>
-    <option value="spider">Spider</option>
-    <option value="goldfish">Goldfish</option>
-  </select>
+  <select class="vf-form__select | selectCustomWidth" id="vf-form__select-site" data-jplist-control="select-filter" data-group="data-group-1">
+    <option value="0" data-path="default" >Select</option>
+    <?php
+    foreach($hrSiteChoices as $value => $label) {
+      ?>
+      <option  
+        data-path=".<?php echo esc_attr($value); ?>" 
+        value="<?php echo esc_attr($value); ?>"><?php echo esc_html($label); ?> </option>
+      <?php } ?>
+    </select>
+
   </div>
 <div class="vf-form__item vf-stack">
 
-  <label class="vf-form__label" for="vf-form__select">Department, team or group</label>
+  <label class="vf-form__label" for="vf-form__select-group">Department, team or group</label>
 
-  <select class="vf-form__select | selectCustomWidth" id="vf-form__select">
-    <option value="cat">Cat</option>
-    <option value="hamster">Hamster</option>
-    <option value="parrot">Parrot</option>
-    <option value="dog" selected>Dog</option>
-    <option value="spider">Spider</option>
-    <option value="goldfish">Goldfish</option>
-  </select>
+  <select class="vf-form__select | selectCustomWidth" id="vf-form__select-group" data-jplist-control="select-filter" data-group="data-group-1">
+    <option value="0" data-path="default" >Select</option>
+    <?php
+    foreach($hrGroupChoices as $value => $label) {
+      ?>
+      <option  
+        data-path=".<?php echo esc_attr($value); ?>" 
+        value="<?php echo esc_attr($value); ?>"><?php echo esc_html($label); ?> </option>
+      <?php } ?>
+    </select>
+
   </div>
 </div>
   <main>
@@ -96,8 +111,11 @@ $today_date = date('Ymd');
 .selectCustomWidth {
   width: 12rem !important;
 }
-</style>
 
+.vf-form__label {
+  font-weight: 500 !important;
+}
+</style>
 
 <script type="text/javascript">
   jplist.init({});
