99fd5f4c328cd3bd0c7a1d6f7d10230b901505c9
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2025-05-13 11:32:19 +0200

    adds front page

---
+++ wp-content/themes/vf-wp-editorhub/front-page.php
@@ -0,0 +1,24 @@
+
+<?php
+
+get_header();
+
+global $vf_theme;
+
+?>
+
+<!--/embl-grid-->
+<section class="embl-grid embl-grid--has-centered-content">
+  <div></div>
+  <div>
+<section class="vf-grid vf-u-margin__bottom--1200 vf-content">
+  <link rel="import" href="https://www.embl.org/api/v1/pattern.html?filter-content-type=article&filter-id=108012&pattern=node-body&source=contenthub" data-target="self" data-embl-js-content-hub-loader>
+</section>
+</div>
+  <div></div>
+</section>
+<?php
+
+get_footer();
+
+?>
