ef9a84c249f671beac48adbff1a0cf0c8985649a
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2026-03-24 11:14:54 +0100

    chatbot template initial

--- wp-content/plugins/vf-events/templates/partials/chatbot.php
+++ wp-content/plugins/vf-events/templates/partials/chatbot.php
@@ -15,47 +15,14 @@
 
   <div class="vf-content vf-chatbot-modal-container vf-chatbot-modal-container--inactive" role="dialog" aria-label="Event Assistant chatbot" data-vf-js-chatbot-modal-container>
     <div role="region" aria-label="Chatbot header" class="vf-chatbot-modal__header">
-      <div role="region" aria-label="Chatbot selector" class="vf-chatbot-modal__header-left">
-        <div
-          class="vf-chatbot-selector"
-          data-vf-js-chatbot-selector
-          data-routes-path="/wp-content/themes/vf-wp/assets/assets/vf-chatbot/assets/vf-chatbot-selector-services.json"
-          data-multiselect="true"
-          data-max-multiselect="3"
-          data-show-search="true"
-          data-show-all-services="true"
-          data-show-all-services-selected="true"
-        >
-          <button class="vf-chatbot-selector__title" data-vf-js-selector-toggle type="button">
+      <div role="region" aria-label="Chatbot title" class="vf-chatbot-modal__header-left">
+        <div class="vf-chatbot-selector">
+          <div class="vf-chatbot-selector__title">
             <img src="/wp-content/themes/vf-wp/assets/assets/vf-chatbot/assets/vf-chatbot--icon-24x24-dark-green.svg" alt="Event Assistant">
             <div class="vf-chatbot-selector__title-content vf-u-margin__left--200">
               <span class="vf-chatbot-selector__main-text">Event Assistant</span>
-              <span class="vf-chatbot-selector__title-text">Services</span>
+              <span class="vf-chatbot-selector__title-text">Custom text</span>
             </div>
-            <span class="vf-chatbot-selector__chevron">
-              <svg width="32" height="31" viewBox="0 0 32 31" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <g clip-path="url(#vf-chatbot-selector-clip)">
-                  <path d="M15.999 19.0975C15.7378 19.098 15.479 19.0468 15.2377 18.9468C14.9963 18.8469 14.7771 18.7001 14.5926 18.5151L8.32863 11.9279C8.21951 11.8137 8.13399 11.6791 8.07698 11.5318C8.01998 11.3845 7.99261 11.2274 7.99645 11.0695C8.00028 10.9116 8.03525 10.756 8.09934 10.6117C8.16342 10.4673 8.25537 10.337 8.36992 10.2283C8.48446 10.1195 8.61934 10.0344 8.76683 9.97791C8.91432 9.92139 9.07152 9.89454 9.2294 9.89889C9.38729 9.90325 9.54277 9.93872 9.68692 10.0033C9.83107 10.0678 9.96106 10.1602 10.0694 10.2751L15.7094 16.2143C15.7467 16.2537 15.7916 16.2851 15.8414 16.3066C15.8912 16.3281 15.9448 16.3391 15.999 16.3391C16.0533 16.3391 16.1069 16.3281 16.1567 16.3066C16.2065 16.2851 16.2514 16.2537 16.2886 16.2143L21.9286 10.2751C22.037 10.1602 22.167 10.0678 22.3112 10.0033C22.4553 9.93872 22.6108 9.90325 22.7687 9.89889C22.9266 9.89454 23.0838 9.92139 23.2312 9.97791C23.3787 10.0344 23.5136 10.1195 23.6282 10.2283C23.7427 10.337 23.8347 10.4673 23.8987 10.6117C23.9628 10.756 23.9978 10.9116 24.0016 11.0695C24.0055 11.2274 23.9781 11.3845 23.9211 11.5318C23.8641 11.6791 23.7786 11.8137 23.6694 11.9279L17.439 18.4991C17.2503 18.6888 17.0259 18.8394 16.7788 18.9421C16.5316 19.0448 16.2667 19.0976 15.999 19.0975Z" fill="#707372"/>
-                </g>
-                <defs>
-                  <clipPath id="vf-chatbot-selector-clip">
-                    <rect width="16" height="16" fill="white" transform="translate(8 6.5)"/>
-                  </clipPath>
-                </defs>
-              </svg>
-            </span>
-          </button>
-
-          <div class="vf-chatbot-selector__dropdown" data-vf-js-selector-dropdown>
-            <div class="vf-chatbot-selector__search">
-              <label class="vf-u-sr-only" id="vf-chatbot-selector-search-label" for="vf-chatbot-selector-search">Type to search</label>
-              <input type="text" id="vf-chatbot-selector-search" aria-labelledby="vf-chatbot-selector-search-label" placeholder="Select services" data-vf-js-selector-search>
-            </div>
-            <div class="vf-chatbot-selector__header">
-              <span data-max-select="3">Select up to 3 services</span>
-              <a href="#" class="vf-chatbot-selector__clear" role="button" data-vf-js-selector-clear>Clear all</a>
-            </div>
-            <ul class="vf-chatbot-selector__list" data-vf-js-chatbot-selector-list></ul>
           </div>
         </div>
       </div>
@@ -70,6 +37,7 @@
     </div>
 
     <div class="vf-chatbot-modal | vf-u-background-color-ui--grey--light" data-vf-js-chatbot-modal>
+         <div id="eventInfo"><div style="zoom: 75%;height: 150px;background-image: url(&quot;https://www.embl.org/about/info/course-and-conference-office/wp-content/uploads/CEL22-01_Hero_banner_3000x1000px_3-scaled-e1689061566403.jpg&quot;);background-position: 73% 46%; /* or 98% 50% */background-size: auto;background-repeat: no-repeat;z-index: 0;"></div></div>
       <div class="vf-chatbot-modal__content" data-vf-js-chatbot-modal-content>
         <div
           role="region"
@@ -80,7 +48,7 @@
           data-enable-qa-data-loading="true"
           data-enable-predefined-qa="true"
           data-enable-fallback-responses="true"
-          data-qa-data-url="/wp-content/themes/vf-wp/assets/assets/vf-chatbot/assets/vf-chatbot-qa.json"
+          data-qa-data-url="/wp-content/themes/vf-wp/assets/assets/chatbot/qa.json"
         >
           <div class="vf-chatbot-welcome__content">
             <div class="vf-chatbot-welcome__logo">
@@ -268,6 +236,37 @@
 <script>
   (function () {
     var assetBase = "/wp-content/themes/vf-wp/assets/assets/vf-chatbot/assets";
+    var assetBaseCustom = "/wp-content/themes/vf-wp/assets/assets/chatbot";
+
+    window.hideEventInfo = function () {
+      var eventInfo = document.getElementById("eventInfo");
+      if (eventInfo) {
+        eventInfo.style.display = "none";
+      }
+    };
+
+    window.showEventInfo = function () {
+      var eventInfo = document.getElementById("eventInfo");
+      if (eventInfo) {
+        eventInfo.style.display = "";
+      }
+    };
+
+    window.handleMessageSend = function () {
+      window.hideEventInfo();
+    };
+
+    window.handleSuggestionClick = function () {
+      window.hideEventInfo();
+    };
+
+    window.handleFabClick = function () {
+      window.showEventInfo();
+    };
+
+    window.handleDialogConfirm = function () {
+      window.showEventInfo();
+    };
 
     window.vfEventChatbotConfig = {
       type: "modal",
@@ -276,8 +275,8 @@
       welcome_message: "Ask about this event, event logistics, or related EMBL resources.",
       welcome_logo_alt: "Event Assistant",
       welcome_suggestions_title: "Try asking me:",
-      input_placeholder: "Ask about this event ...",
-      welcome_max_suggestions: 4,
+      input_placeholder: "Ask about this event",
+      welcome_max_suggestions: 3,
       icons: {
         assistant_avatar: assetBase + "/vf-chatbot--icon-16x16-dark-green.svg",
         user_avatar: assetBase + "/vf-chatbot--avatar-user.svg",
@@ -289,7 +288,7 @@
       api: {
         chat_endpoint: false,
         feedback_endpoint: false,
-        qa_data_url: assetBase + "/vf-chatbot-qa.json",
+        qa_data_url: assetBaseCustom + "/qa.json",
         headers: {
           "Content-Type": "application/json"
         },
@@ -313,20 +312,11 @@
         typing_delay: 800,
         show_scrollbar: false
       },
-      selectorContext: {
-        chatbotRoutes: {
-          multiSelect: true,
-          maxMultiSelect: 3,
-          showSearch: true,
-          showSearchThreshold: 5,
-          showAllServices: true,
-          showAllServicesSelected: true,
-          routes: assetBase + "/vf-chatbot-selector-services.json",
-          placeholder: "Select services",
-          title: "Services",
-          selector_logo_url: assetBase + "/vf-chatbot--icon-24x24-dark-green.svg",
-          selector_logo_title: "Event Assistant"
-        }
+      handlers: {
+        on_message_send: "handleMessageSend",
+        on_suggestion_click: "handleSuggestionClick",
+        on_fab_click: "handleFabClick",
+        on_dialog_confirm: "handleDialogConfirm"
       },
       feedback_options: {
         positive: [
@@ -349,3 +339,8 @@
     window.config = window.vfEventChatbotConfig;
   })();
 </script>
+<style>
+    .vf-chatbot-modal {
+        justify-content: space-between;
+            }
+</style>
