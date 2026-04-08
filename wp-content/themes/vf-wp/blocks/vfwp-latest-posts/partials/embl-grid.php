ae0a180ecfb3717e6d752c115daa284b70c9a435
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2026-04-08 11:17:00 +0200

    fix: latest posts block

--- wp-content/themes/vf-wp/blocks/vfwp-latest-posts/partials/embl-grid.php
+++ wp-content/themes/vf-wp/blocks/vfwp-latest-posts/partials/embl-grid.php
@@ -1,185 +1,100 @@
 <?php
- include(locate_template('blocks/vfwp-latest-posts/partials/loops/loops.php', false, false)); 
+include( locate_template( 'blocks/vfwp-latest-posts/partials/loops/loops.php', false, false ) );
+
+if ( ! isset( $loopPost ) || ! ( $loopPost instanceof WP_Query ) || ! $loopPost->have_posts() ) {
+  return;
+}
 
 $ids = array();
-while ($loopPost->have_posts()) : $loopPost->the_post();
-$ids[] = get_the_ID();
-$post_id = get_the_ID();
-
+while ( $loopPost->have_posts() ) :
+  $loopPost->the_post();
+  $ids[] = get_the_ID();
+  $post_id = get_the_ID();
+  $current_post_type = get_post_type();
+  $event_data = 'events' === $current_post_type ? vfwp_latest_posts_get_event_display_data( $post_id ) : null;
 ?>
 
 <article class="vf-summary vf-summary--news">
-  <?php if (get_post_type() === 'events') { 
-  $start_date = get_field('vf_event_internal_start_date',$post_id);
-  $start_time = get_field('vf_event_internal_start_time',$post_id);
-  $start = DateTime::createFromFormat('j M Y', $start_date);
-  $start_time_format = DateTime::createFromFormat('H:i', $start_time);
-  $end_date = get_field('vf_event_internal_end_date',$post_id);
-  $end_time = get_field('vf_event_internal_end_time',$post_id);
-  $end_time_format = DateTime::createFromFormat('H:i', $end_time);
-  $end = DateTime::createFromFormat('j M Y', $end_date);
-  $end_date_format = DateTime::createFromFormat('j M Y', $end_date);
-  $customDateSorting = DateTime::createFromFormat('Ymd', $start_time);
-  
-  if (!empty($start_time)) {
-    $calendar_start_time = 'T' . $start_time_format->format('Hi') . '00';
-  }
-  else {
-    $calendar_start_time = '';
-  }
-  
-  if (!empty($end_time)) {
-    $calendar_end_time = 'T' . $end_time_format->format('Hi') . '00';
-  }
-  elseif (empty($end_time) && !empty($start_time)) {
-    $calendar_end_time = 'T' . $start_time_format->format('Hi') . '00';
-  }
-  else {
-    $calendar_end_time = '';
-  }
-  
-  if (!empty($end_date)) {
-    $calendar_end_date = '/' . $end->format('Ymd');
-  }
-  else {
-    $calendar_end_date = '/' . $start->format('Ymd');
-  }
-  ?>
-  <?php if ( ! empty($start_date)) { ?>
-  <p class="vf-summary__date" data-eventtime="<?php echo $customDateSorting; ?>">
-    <?php       // Event dates
-        if ($end_date) { 
-          if ($start->format('M') == $end->format('M')) {
-            echo $start->format('j'); ?> - <?php echo $end->format('j F Y'); }
-          else {
-            echo $start->format('j M'); ?> - <?php echo $end->format('j F Y'); }
-      ?>
-    <?php } 
-        else {
-          echo $start->format('j F Y'); 
-        } 
-        
-      ?>
+  <?php if ( 'events' === $current_post_type && ! empty( $event_data['has_date'] ) ) : ?>
+  <p class="vf-summary__date" data-eventtime="<?php echo esc_attr( $event_data['sort_key'] ); ?>">
+    <?php echo esc_html( $event_data['display_date'] ); ?>
     &nbsp;&nbsp;
     <span class="vf-text-body vf-text-body--5 | vf-u-margin__bottom--100" style="text-transform: none;">
-      <a href="http://www.google.com/calendar/render?action=TEMPLATE&text=<?php the_title(); ?>&dates=<?php echo $start->format('Ymd') . $calendar_start_time; ?><?php echo $calendar_end_date . $calendar_end_time; ?>&sprop=name:"
-        target="_blank" rel="nofollow">Add to calendar</a>
+      <a href="<?php echo esc_url( $event_data['calendar_url'] ); ?>" target="_blank" rel="nofollow">Add to calendar</a>
     </span>
   </p>
-  <?php } }
-  else {
-  ?>
-
-  <span class="vf-summary__date" title="<?php the_time('c'); ?>"
-    datetime="<?php the_time('c'); ?>"><?php the_time(get_option('date_format')); ?>
+  <?php elseif ( 'events' !== $current_post_type ) : ?>
+  <span class="vf-summary__date" title="<?php the_time( 'c' ); ?>" datetime="<?php the_time( 'c' ); ?>">
+    <?php the_time( get_option( 'date_format' ) ); ?>
   </span>
-  <?php } ?>
-  <?php if ($show_image == 1) { ?>
-  <?php 
-  if ( has_post_thumbnail() ) {
-    the_post_thumbnail( 'full', array( 'class' => 'vf-summary__image', 'style' => 'height: auto;' ) ); 
-  }
-  else { 
-    if ( 'community-blog' == get_post_type() ) { ?>
+  <?php endif; ?>
+
+  <?php if ( 1 == $show_image ) : ?>
+    <?php if ( has_post_thumbnail() ) : ?>
+      <?php the_post_thumbnail( 'full', array( 'class' => 'vf-summary__image', 'style' => 'height: auto;' ) ); ?>
+    <?php elseif ( 'community-blog' === $current_post_type ) : ?>
   <img class="vf-summary__image"
     src="https://www.embl.org/internal-information/wp-content/uploads/Announcementes-and-updates.jpg" alt="Placeholder"
     loading="lazy">
-  <?php } } } ?>
+    <?php endif; ?>
+  <?php endif; ?>
+
   <h3 class="vf-summary__title">
-    <a href="<?php the_permalink(); ?>" class="vf-summary__link"><?php echo esc_html(get_the_title()); ?></a>
+    <a href="<?php the_permalink(); ?>" class="vf-summary__link"><?php echo esc_html( get_the_title() ); ?></a>
   </h3>
-  <?php if ($show_excerpt) { ?>
+
+  <?php if ( $show_excerpt ) : ?>
   <p class="vf-summary__text">
-    <?php
-     echo get_the_excerpt(); ?>
+    <?php echo wp_kses_post( get_the_excerpt() ); ?>
   </p>
-  <?php
-   } ?>
-  <?php if ($show_categories == 1) { ?>
+  <?php endif; ?>
+
+  <?php if ( 1 == $show_categories ) : ?>
   <p class="vf-summary__text">
     <span class="vf-summary__category">
-      <?php echo get_the_category_list(', '); ?>
-    </span></p> <?php } ?>
-  <?php if ($show_topics == 1) { ?>
+      <?php echo get_the_category_list( ', ' ); ?>
+    </span>
+  </p>
+  <?php endif; ?>
+
+  <?php if ( 1 == $show_topics ) : ?>
   <p class="vf-summary__text | vf-u-margin__bottom--0">
     <span class="vf-summary__meta">
-      <?php if (get_post_type() === 'insites') { 
-  $topic_terms = get_the_terms( $post->ID, 'topic' );
-  if( $topic_terms ) {
-    $topics_list = array(); 
-    foreach( $topic_terms as $term ) {
-      $topics_list[] = '<a class="vf-link ' . esc_attr( $term->slug ) . '"style="color: #707372;" href="' . esc_url(get_term_link( $term )) . '">' . strtoupper(esc_html( $term->name )) . '</a>'; }
-      echo implode(', ', $topics_list); }?> </span>
-    <?php } ?>
-    <?php if (get_post_type() === 'community-blog') { 
-  $topic_terms = get_the_terms( $post->ID, 'updates-topic' );
-  if( $topic_terms ) {
-    $topics_list = array(); 
-    foreach( $topic_terms as $term ) {
-      $topics_list[] = '<a class="vf-link ' . esc_attr( $term->slug ) . '"style="color: #707372;" href="' . esc_url(get_term_link( $term )) . '">' . strtoupper(esc_html( $term->name )) . '</a>'; }
-      echo implode(', ', $topics_list); }?> </span>
-    <?php } ?>
-    <?php if (get_post_type() === 'events') { 
-  $topic_terms = get_the_terms( $post->ID, 'events-topic' );
-  if( $topic_terms ) {
-    $topics_list = array(); 
-    foreach( $topic_terms as $term ) {
-      $topics_list[] = '<a class="vf-link ' . esc_attr( $term->slug ) . '"style="color: #707372;" href="' . esc_url(get_term_link( $term )) . '">' . strtoupper(esc_html( $term->name )) . '</a>'; }
-      echo implode(', ', $topics_list); }?> </span>
-    <?php } ?>
-  </p>
-  <?php } 
-  ?>
-  <?php if ($show_location == 1) { ?>
-  <p class="vf-summary__text">
-    <?php if (get_post_type() === 'community-blog' || get_post_type() === 'insites') { 
-      $locations = get_the_terms( $post->ID, 'embl-location' );
-      if (($locations)) { ?>
-    <span class="vf-text-body vf-text-body--5
-       location vf-u-margin__top--0"><span style="font-weight: 500;">EMBL site:</span>
-      <?php $location_list = [];
-              foreach( $locations as $location ) { 
-                switch ($location->name) {
-                  case "Heidelberg":
-                    $location->name = 'HD';
-                    break;
-                  case "Hamburg":
-                    $location->name = 'HH';
-                    break;
-                  case "Rome":
-                    $location->name = 'RM';
-                    break;
-                  case "Grenoble":
-                    $location->name = 'GR';
-                    break;
-                  case "Barcelona":
-                    $location->name = 'BCN';
-                    break;
-                  case "EMBL-EBI":
-                    $location->name = 'EBI';
-                     break;
-                  }
-                $location_list[] = $location->name; }
-                echo implode(', ', $location_list); ?>
-    </span>
-
-    <?php      
-     } } 
-     if (get_post_type() === 'events') { 
-      $event_location_terms = get_the_terms( $post->ID, 'event-location' );
-      if( $event_location_terms ) { ?>
-    <span class="vf-text-body vf-text-body--5 location vf-u-margin__top--0">
       <?php
-        $event_locations_list = array(); 
-        foreach( $event_location_terms as $term ) {
-          $event_locations_list[] = esc_html( $term->name ); }
-          echo implode(', ', $event_locations_list); }?>
+      if ( 'insites' === $current_post_type ) {
+        echo vfwp_latest_posts_get_term_links( $post_id, 'topic' );
+      } elseif ( 'community-blog' === $current_post_type ) {
+        echo vfwp_latest_posts_get_term_links( $post_id, 'updates-topic' );
+      } elseif ( 'events' === $current_post_type ) {
+        echo vfwp_latest_posts_get_term_links( $post_id, 'events-topic' );
+      }
+      ?>
     </span>
-    <?php } ?>
   </p>
-  <?php } ?>
+  <?php endif; ?>
 
+  <?php if ( 1 == $show_location ) : ?>
+  <p class="vf-summary__text">
+    <?php if ( 'community-blog' === $current_post_type || 'insites' === $current_post_type ) : ?>
+      <?php $location_list = vfwp_latest_posts_get_term_names( $post_id, 'embl-location', true ); ?>
+      <?php if ( ! empty( $location_list ) ) : ?>
+    <span class="vf-text-body vf-text-body--5 location vf-u-margin__top--0"><span style="font-weight: 500;">EMBL site:</span>
+      <?php echo esc_html( implode( ', ', $location_list ) ); ?>
+    </span>
+      <?php endif; ?>
+    <?php endif; ?>
+
+    <?php if ( 'events' === $current_post_type ) : ?>
+      <?php $event_locations_list = vfwp_latest_posts_get_term_names( $post_id, 'event-location' ); ?>
+      <?php if ( ! empty( $event_locations_list ) ) : ?>
+    <span class="vf-text-body vf-text-body--5 location vf-u-margin__top--0">
+      <?php echo esc_html( implode( ', ', $event_locations_list ) ); ?>
+    </span>
+      <?php endif; ?>
+    <?php endif; ?>
+  </p>
+  <?php endif; ?>
 </article>
 <!--/vf-summary-->
-<?php endwhile;?>
+<?php endwhile; ?>
 <?php wp_reset_postdata(); ?>
--- wp-content/themes/vf-wp/blocks/vfwp-latest-posts/template.php
+++ wp-content/themes/vf-wp/blocks/vfwp-latest-posts/template.php
@@ -1,119 +1,103 @@
 <?php
 
-$is_container = get_field('is_container');
-// Fallback for undefined older block fields
-if ($is_container === null) {
+$vfwp_latest_posts_helpers = locate_template( 'blocks/vfwp-latest-posts/partials/helpers.php', false, false );
+
+if ( $vfwp_latest_posts_helpers ) {
+  require_once $vfwp_latest_posts_helpers;
+}
+
+$is_container = get_field( 'is_container' );
+if ( null === $is_container ) {
   $is_container = true;
 }
 $is_container = (bool) $is_container;
 
-$title = get_the_title();
-$theme = wp_get_theme();
-$limit = get_field('limit');
-$post_type = get_field('post_type');
-$show_topics = get_field('show_topics');
-$show_excerpt = get_field('show_excerpt');
-$show_heading = get_field('show_heading');
+$limit = get_field( 'limit' );
+$post_type = get_field( 'post_type' );
+$show_topics = get_field( 'show_topics' );
+$show_excerpt = get_field( 'show_excerpt' );
+$show_heading = get_field( 'show_heading' );
 
-$heading_singular = get_field('heading_singular');
-$heading_singular = !empty($heading_singular) ? trim($heading_singular) : __('Latest posts', 'vfwp');
+$heading_singular = get_field( 'heading_singular' );
+$heading_singular = ! empty( $heading_singular ) ? trim( $heading_singular ) : __( 'Latest posts', 'vfwp' );
 
-$heading_text = get_field('heading_text');
-$heading_url = get_field('heading_url');
+$heading_text = get_field( 'heading_text' );
+$heading_url = get_field( 'heading_url' );
 
-// Safely determine heading link and target
 $heading_link = '';
 $heading_target = '_self';
 
-if (!empty($heading_url) && is_array($heading_url)) {
-    // ACF URL array format
-    $heading_link = $heading_url['url'] ?? '';
-    $heading_target = $heading_url['target'] ?? '_self';
-} elseif (!empty($heading_url) && is_string($heading_url)) {
-    // Legacy string value
-    $heading_link = $heading_url;
-    $heading_target = '_self';
+if ( ! empty( $heading_url ) && is_array( $heading_url ) ) {
+  $heading_link = $heading_url['url'] ?? '';
+  $heading_target = $heading_url['target'] ?? '_self';
+} elseif ( ! empty( $heading_url ) && is_string( $heading_url ) ) {
+  $heading_link = $heading_url;
 } else {
-    // Fallback
-    if ($theme === 'VF-WP Intranet') {
-        $heading_link = '/community-blog';
-    } else {
-        $heading_link = get_permalink(get_option('page_for_posts'));
-    }
+  $heading_link = vfwp_latest_posts_get_default_heading_link( $post_type );
 }
 
-if (empty($post_type)) {
+if ( empty( $post_type ) ) {
   $post_type = 'post';
 }
 
-$layout = get_field('layout');
-$grid = get_field('grid');
+$layout = get_field( 'layout' );
+$grid = get_field( 'grid' );
 
-$category = get_field('category');
-$tag = get_field('tag');
+$category = get_field( 'category' );
+$tag = get_field( 'tag' );
 
-$show_image = get_field('show_image');
-$show_location = get_field('show_location');
-$show_categories = get_field('show_categories');
+$show_image = get_field( 'show_image' );
+$show_location = get_field( 'show_location' );
+$show_categories = get_field( 'show_categories' );
 
-$latest_posts = get_posts(array(
-  'posts_per_page' => 3
-));
+$vf_grid = '';
+$embl_grid = '';
+$has_outer_container = $is_container && ( ( 'list' === $layout && $show_heading ) || 'embl-grid' === $grid );
 
-if (count($latest_posts)) {
-    // Setup first post data so template tags work
-    global $post;
-    $old_post = $post;
-    $post = $latest_posts[0];
-    setup_postdata($post);
-
-    $vf_grid = '';
-    $embl_grid = '';
-
-    if ($layout === 'columns') {
-        $embl_grid = null;
-        $vf_grid = 'class="vf-grid vf-grid__col-3 | vf-content"';
-    } else {
-        $embl_grid = 'embl-grid--has-centered-content';
-    }
+if ( 'columns' === $layout ) {
+  $embl_grid = null;
+  $vf_grid = 'class="vf-grid vf-grid__col-4 | vf-content"';
+} else {
+  $embl_grid = 'embl-grid--has-centered-content';
+}
 ?>
 
-<?php if ($is_container && ((( $layout === 'list' && $show_heading ) || $grid === 'embl-grid' ))) : ?>
-<div class="embl-grid <?php echo esc_attr($embl_grid); ?> | vf-content">
-    <?php if (!empty($heading_singular)) : ?>
+<?php if ( $has_outer_container ) : ?>
+<div class="embl-grid <?php echo esc_attr( $embl_grid ); ?> | vf-content">
+    <?php if ( ! empty( $heading_singular ) ) : ?>
     <div class="vf-section-header">
         <h2 class="vf-section-header__heading">
             <a class="vf-section-header__heading vf-section-header__heading--is-link"
-               target="<?php echo esc_attr($heading_target); ?>"
-               href="<?php echo esc_url($heading_link); ?>">
-                <?php echo esc_html($heading_singular); ?>
+               target="<?php echo esc_attr( $heading_target ); ?>"
+               href="<?php echo esc_url( $heading_link ); ?>">
+                <?php echo esc_html( $heading_singular ); ?>
                 <svg aria-hidden="true" class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path>
                 </svg>
             </a>
         </h2>
-        <?php if (!empty($heading_text)) : ?>
-        <p class="vf-section-header__text"><?php echo esc_html($heading_text); ?></p>
+        <?php if ( ! empty( $heading_text ) ) : ?>
+        <p class="vf-section-header__text"><?php echo esc_html( $heading_text ); ?></p>
         <?php endif; ?>
     </div>
     <?php endif; ?>
 <?php endif; ?>
 
-<?php if ($is_container && $grid === 'vf-grid') : ?>
-    <?php if (!empty($heading_singular)) : ?>
+<?php if ( $is_container && 'vf-grid' === $grid ) : ?>
+    <?php if ( ! empty( $heading_singular ) ) : ?>
     <div class="vf-section-header">
         <h2 class="vf-section-header__heading">
             <a class="vf-section-header__heading vf-section-header__heading--is-link"
-               target="<?php echo esc_attr($heading_target); ?>"
-               href="<?php echo esc_url($heading_link); ?>">
-                <?php echo esc_html($heading_singular); ?>
+               target="<?php echo esc_attr( $heading_target ); ?>"
+               href="<?php echo esc_url( $heading_link ); ?>">
+                <?php echo esc_html( $heading_singular ); ?>
                 <svg aria-hidden="true" class="vf-section-header__icon | vf-icon vf-icon-arrow--inline-end" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z" fill="" fill-rule="nonzero"></path>
                 </svg>
             </a>
         </h2>
-        <?php if (!empty($heading_text)) : ?>
-        <p class="vf-section-header__text"><?php echo esc_html($heading_text); ?></p>
+        <?php if ( ! empty( $heading_text ) ) : ?>
+        <p class="vf-section-header__text"><?php echo esc_html( $heading_text ); ?></p>
         <?php endif; ?>
     </div>
     <?php endif; ?>
@@ -121,23 +105,16 @@ if (count($latest_posts)) {
 
 <div <?php echo $vf_grid; ?>>
 
-    <?php if ($layout === 'list') : ?>
-        <?php include(locate_template('blocks/vfwp-latest-posts/partials/embl-grid.php', false, false)); ?>
+    <?php if ( 'list' === $layout ) : ?>
+        <?php include( locate_template( 'blocks/vfwp-latest-posts/partials/embl-grid.php', false, false ) ); ?>
     <?php endif; ?>
 
-    <?php if ($layout === 'columns') : ?>
-        <?php include(locate_template('blocks/vfwp-latest-posts/partials/columns-grid.php', false, false)); ?>
+    <?php if ( 'columns' === $layout ) : ?>
+        <?php include( locate_template( 'blocks/vfwp-latest-posts/partials/columns-grid.php', false, false ) ); ?>
     <?php endif; ?>
 
 </div>
 
-<?php if ($is_container) : ?>
+<?php if ( $has_outer_container ) : ?>
 </div>
 <?php endif; ?>
-
-<?php
-// Reset post data back to plugin
-$post = $old_post;
-setup_postdata($post);
-}
-?>
