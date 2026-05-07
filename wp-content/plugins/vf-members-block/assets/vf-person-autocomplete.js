fb05fdba25117a4eb98563bf6fd27d6f8597b8a1
Szymon Kasprzyk <szymon.kasprzyk@embl.de>
2026-05-07 08:39:31 +0200

    restores enter a search query banner for vf-person

--- wp-content/plugins/vf-members-block/assets/vf-person-autocomplete.js
+++ wp-content/plugins/vf-members-block/assets/vf-person-autocomplete.js
@@ -4,6 +4,7 @@
   }
 
   var config = window.vfPersonAutocomplete;
+  var helperSelector = '.vf-person-record-select-helper';
 
   function getScope(field) {
     if (field.classList.contains('vf-person-scope-internal')) {
@@ -11,6 +12,48 @@
     }
 
     return 'public';
+  }
+
+  function getHelper($field) {
+    var $helper = $field.find(helperSelector);
+
+    if ($helper.length) {
+      return $helper;
+    }
+
+    $helper = $(
+      '<div class="vf-person-record-select-helper vf-banner vf-banner--alert vf-banner--info">' +
+        '<div class="vf-banner__content">' +
+          '<p class="vf-banner__text">Enter a search query to find a person.</p>' +
+        '</div>' +
+      '</div>'
+    );
+
+    $field.append($helper);
+
+    return $helper;
+  }
+
+  function hasValue($select) {
+    var value = $select.val();
+
+    if (Array.isArray(value)) {
+      value = value[0];
+    }
+
+    return !!(value && String(value).trim());
+  }
+
+  function updateHelper($field, isSearching) {
+    var $select = $field.find('select');
+    var $helper = getHelper($field);
+
+    if (hasValue($select) || isSearching) {
+      $helper.hide();
+      return;
+    }
+
+    $helper.show();
   }
 
   function setupField(field) {
@@ -24,6 +67,8 @@
     if ($select.data('select2')) {
       $select.select2('destroy');
     }
+
+    $select.off('.vfPersonAutocomplete');
 
     $select.select2({
       ajax: {
@@ -66,8 +111,26 @@
       width: '100%'
     });
 
+    $select.on('select2:open.vfPersonAutocomplete', function() {
+      updateHelper($field, true);
+
+      window.setTimeout(function() {
+        var $search = $('.select2-container--open .select2-search__field');
+
+        $search.off('.vfPersonAutocomplete');
+        $search.on('input.vfPersonAutocomplete', function() {
+          updateHelper($field, !!this.value.trim());
+        });
+      }, 0);
+    });
+
+    $select.on('select2:close.vfPersonAutocomplete change.vfPersonAutocomplete', function() {
+      updateHelper($field, false);
+    });
+
     $select.attr('data-placeholder', 'Search for a person');
     $field.attr('data-vf-person-record-select-ready', '1');
+    updateHelper($field, false);
   }
 
   function getRoot(context) {
